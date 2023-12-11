e8cd92752c60509a23b8f417f5bcdeb062745245
asml <p80@80q.ru>
2023-12-11 22:32:25 +0300

    +

--- viktorinaonlinebot/src/components/ErrorPage.tsx
+++ viktorinaonlinebot/src/components/ErrorPage.tsx
@@ -8,7 +8,7 @@ export default function ErrorPage() {
             className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
           />
           <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-            <p className="text-base font-semibold leading-8 text-white">Ошибка</p>
+            <p className="text-base font-semibold leading-8 text-white">Ошибка !</p>
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Технические работы</h1>
             <p className="mt-4 text-base text-white/70 sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
             <div className="mt-10 flex justify-center">
