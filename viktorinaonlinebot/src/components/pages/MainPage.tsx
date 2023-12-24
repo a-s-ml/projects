ca96ba1f5b1d4dc7a2b2187204faee913631b400
asml <p80@80q.ru>
2023-12-24 23:51:32 +0300

    +

--- viktorinaonlinebot/src/components/pages/MainPage.tsx
+++ viktorinaonlinebot/src/components/pages/MainPage.tsx
@@ -34,7 +34,7 @@ export function MainPage() {
               <img className="mx-auto h-12 w-12" src={Logo} />
 
               <h2 className="mt-2 text-base font-semibold leading-6 text-[var(--tg-theme-accent-text-color)]">
-                ViktorinaOnlineBot
+                ViktorinaOnlineBot!!!
               </h2>
               <p className="mt-1 text-sm text-[var(--tg-theme-hint-color)]">
                 Бот проводит викторины в ваших группах/каналах используя
