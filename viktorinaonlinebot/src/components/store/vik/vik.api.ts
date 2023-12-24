b4a1f7691c91d25144814369ae4835e966e00d4f
asml <p80@80q.ru>
2023-12-24 23:45:01 +0300

    +

--- viktorinaonlinebot/src/components/store/vik/vik.api.ts
+++ viktorinaonlinebot/src/components/store/vik/vik.api.ts
@@ -4,7 +4,7 @@ import { IUser } from "../../../models/IUser";
 export const vikApi = createApi({
   reducerPath: "vikApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api80q.ru/viktorinaonlinebot",
+    baseUrl: "https://api80q.ru/viktorinaonlinebot/",
   }),
   endpoints: (build) => ({
     validate: build.query<IUser[], string>({
