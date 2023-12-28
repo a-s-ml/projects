645adee3820309f8ea9c334b15b9fdb8bb22d7d1
asml <p80@80q.ru>
2023-12-28 23:52:28 +0300

    +

--- viktorinaonlinebot/src/components/GroupAvatar.tsx
+++ viktorinaonlinebot/src/components/GroupAvatar.tsx
@@ -2,7 +2,7 @@ export default function GroupAvatar() {
   return (
     <img
       className="inline-block h-14 w-14 rounded-full"
-      src="https://api.telegram.org/file/bot741882957:AAG7v4E7lAjXBc6BGjP9qmEFFniRoyc61nw/photos/file_0.jpg"
+      src="https://api80q.ru/viktorinaonlinebot/chat/tgGetFilePhoto/AgACAgIAAxUAAWWNquxlSXsUGiwQ-TyWspUmYYfRAAKqpzEb31MbH_Whf4N3K1nbAQADAgADYwADMwQ"
       alt=""
     />
   );
