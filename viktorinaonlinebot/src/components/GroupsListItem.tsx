cccf50b880d2c06fbb1301cb5c5d965ea266b158
asml <p80@80q.ru>
2023-12-28 16:56:42 +0300

    +

--- viktorinaonlinebot/src/components/GroupsListItem.tsx
+++ viktorinaonlinebot/src/components/GroupsListItem.tsx
@@ -1,5 +1,8 @@
 import { ChevronRightIcon, HomeIcon } from "@heroicons/react/20/solid";
-import { useGetInfoGroupsQuery, useGetMemberCountGroupsQuery } from "./store/api/groupsSlise";
+import {
+  useGetInfoGroupsQuery,
+  useGetMemberCountGroupsQuery,
+} from "./store/api/groupsSlise";
 
 interface GroupsListItemProps {
   group: number;
@@ -26,37 +29,45 @@ export default function GroupsListItem({ group }: GroupsListItemProps) {
   }
 
   return (
-    <li>
-      <div className="group relative flex items-start space-x-3 py-4">
-        <div className="flex-shrink-0">
-          <span className="inline-flex h-10 w-10 items-center justify-center rounded-lg">
-            <HomeIcon
-              className="h-6 w-6 text-[var(--tg-theme-accent-text-color)]"
-              aria-hidden="true"
-            />
-          </span>
-        </div>
-        <div className="min-w-0 flex-1">
-          <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
-            <b>
-              <span className="absolute inset-0" aria-hidden="true" />
-              {dataGroupInfo?.title}
-            </b>
+    <>
+      {errorGroupInfo && <li>error</li>}
+      {dataGroupInfo && (
+        <li>
+          <div className="group relative flex items-start space-x-3 py-4">
+            <div className="flex-shrink-0">
+              <span className="inline-flex h-10 w-10 items-center justify-center rounded-lg">
+                <HomeIcon
+                  className="h-6 w-6 text-[var(--tg-theme-accent-text-color)]"
+                  aria-hidden="true"
+                />
+              </span>
+            </div>
+            <div className="min-w-0 flex-1">
+              <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
+                <b>
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  {dataGroupInfo.title}
+                </b>
+              </div>
+              <p className="text-sm text-[var(--tg-theme-hint-color)]">
+                {dataGroupInfo.username}
+              </p>
+              {errorGroupMemberCount && <p>error</p>}
+              {dataGroupMemberCount && (
+                <p className="text-sm text-[var(--tg-theme-hint-color)]">
+                  {dataGroupMemberCount}
+                </p>
+              )}
+            </div>
+            <div className="flex-shrink-0 self-center">
+              <ChevronRightIcon
+                className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
+                aria-hidden="true"
+              />
+            </div>
           </div>
-          <p className="text-sm text-[var(--tg-theme-hint-color)]">
-            {dataGroupInfo?.username}
-          </p>
-          <p className="text-sm text-[var(--tg-theme-hint-color)]">
-          {dataGroupMemberCount}
-          </p>
-        </div>
-        <div className="flex-shrink-0 self-center">
-          <ChevronRightIcon
-            className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
-            aria-hidden="true"
-          />
-        </div>
-      </div>
-    </li>
+        </li>
+      )}
+    </>
   );
 }
