ccca89dedfa0899db2d51c8b49625eecb2e61a6f
asml <p80@80q.ru>
2023-12-28 19:08:14 +0300

    +

--- viktorinaonlinebot/src/components/pages/MainPage.tsx
+++ viktorinaonlinebot/src/components/pages/MainPage.tsx
@@ -44,13 +44,11 @@ export function MainPage() {
     setSlideState(!slideState);
     if (!slideState) {
       tg.BackButton.hide();
-      tg.offEvent('backButtonClicked', setSlideState(false))
       setSlideData(s);
     }
     if (slideState) {
       tg.BackButton.show();
       tg.onEvent('backButtonClicked', setSlideState(false))
-      setSlideData(s);
     }
   }
 
