a4a29103418242e741e0875f92b5ad0ebe470d30
asml <p80@80q.ru>
2023-12-29 04:54:23 +0300

    +

--- viktorinaonlinebot/src/components/GroupAvatar.tsx
+++ viktorinaonlinebot/src/components/GroupAvatar.tsx
@@ -1,28 +1,29 @@
 import { useGetFilePhotoQuery } from "./store/api/groupsSlise";
 
 interface GroupAvatarProps {
-    id: string;
+  id: string;
+}
+
+export default function GroupAvatar({ id }: GroupAvatarProps) {
+  const {
+    isLoading: loadPhoto,
+    isError: errorPhoto,
+    data: dataPhoto,
+  } = useGetFilePhotoQuery(id);
+
+  if (dataPhoto) {
+    console.log(dataPhoto);
   }
 
-export default function GroupAvatar( {id}: GroupAvatarProps) {
-
-    console.log(id)
-
-    const {
-        isLoading: loadPhoto,
-        isError: errorPhoto,
-        data: dataPhoto,
-      } = useGetFilePhotoQuery(id);
-    
-      if(dataPhoto) {
-        console.log(dataPhoto)
-      }
-
   return (
-    <img
-      className="inline-block h-14 w-14 rounded-full"
-      src="data:image/png;base64,https://api80q.ru/viktorinaonlinebot/chat/tgGetFilePhoto/AQADAgADO8MxG1DlIEsACAMAA4qo_sIW____hHpcgxmvTlU0BA"
-      alt="Проверка фото"
-    />
+    <>
+      {dataPhoto && (
+        <img
+          className="inline-block h-14 w-14 rounded-full"
+          src="data:image/png;base64,https://api80q.ru/viktorinaonlinebot/chat/tgGetFilePhoto/"
+          alt="Проверка фото"
+        />
+      )}
+    </>
   );
 }
