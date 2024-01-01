bdd5ddd58a683cfdee19977edef92213838700b3
asml <p80@80q.ru>
2024-01-01 18:37:08 +0300

    +

--- viktorinaonlinebot/src/components/pages/SlidePage.tsx
+++ viktorinaonlinebot/src/components/pages/SlidePage.tsx
@@ -3,35 +3,40 @@ import { Dialog, Transition } from "@headlessui/react";
 import ErrorPage from "../ErrorPage";
 import GroupsList from "../GroupsList";
 import QuestionsList from "../QuestionsList";
-import { selectSlide, selectSlideData, showSlide } from "../store/api/slide.slice";
+import {
+  selectSlide,
+  selectSlideData,
+  showSlide,
+} from "../store/api/slide.slice";
 import { useAppDispatch, useAppSelector } from "../store";
 
 interface SlideItemsProps {
   chat: number;
 }
 
-export default function SlidePage({
-  chat,
-}: SlideItemsProps) {
-
+export default function SlidePage({ chat }: SlideItemsProps) {
   const slide = useAppSelector(selectSlide);
   const slideData = useAppSelector(selectSlideData);
   const dispatch = useAppDispatch();
   const tg = window.Telegram.WebApp;
 
-  if(slide) {
+  if (slide === false) {
     tg.BackButton.show();
     tg.onEvent("backButtonClicked", () => dispatch(showSlide(false)));
   }
-  if(!slide) {
-    tg.BackButton.show();
+  if (slide === true) {
+    tg.BackButton.hide();
     tg.offEvent("backButtonClicked", () => dispatch(showSlide(false)));
   }
 
   return (
     <>
       <Transition.Root show={slide} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={() => dispatch(showSlide(false))}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => dispatch(showSlide(false))}
+        >
           <div className="fixed inset-0 overflow-hidden">
             <div className="absolute inset-0 overflow-hidden">
               <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full">
