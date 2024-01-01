8aa2562d23af19b959b84f812c0a48e2a6589077
asml <p80@80q.ru>
2024-01-01 21:43:49 +0300

    +

--- viktorinaonlinebot/src/components/MenuList.tsx
+++ viktorinaonlinebot/src/components/MenuList.tsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, HomeIcon, UsersIcon, QuestionMarkCircleIcon, QueueListIcon } from "@heroicons/react/20/solid";
+import { ChevronRightIcon, UsersIcon, QuestionMarkCircleIcon, QueueListIcon } from "@heroicons/react/20/solid";
 import { IMenu } from "../models/IMenu";
 import { IProgressData } from "../models/IUser";
 import { useAppDispatch } from "./store";
@@ -24,10 +24,24 @@ export default function MenuList({ item }: MenuItemsProps) {
       <div className="group relative flex items-start space-x-3 py-4">
         <div className="flex-shrink-0">
           <span className="inline-flex h-10 w-10 items-center justify-center rounded-lg">
+          {item.name=='groups' && (
             <UsersIcon
               className="h-6 w-6 text-[var(--tg-theme-accent-text-color)]"
               aria-hidden="true"
             />
+          )}
+          {item.name=='questions' && (
+            <QuestionMarkCircleIcon
+              className="h-6 w-6 text-[var(--tg-theme-accent-text-color)]"
+              aria-hidden="true"
+            />
+          )}
+          {item.name=='answers' && (
+            <QueueListIcon
+              className="h-6 w-6 text-[var(--tg-theme-accent-text-color)]"
+              aria-hidden="true"
+            />
+          )}
           </span>
         </div>
         <div className="min-w-0 flex-1">
