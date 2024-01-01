6737d4e555a3d30765a4cb90de7d8efa67c1a8c7
asml <p80@80q.ru>
2024-01-01 17:03:15 +0300

    +

--- viktorinaonlinebot/src/components/store/index.ts
+++ viktorinaonlinebot/src/components/store/index.ts
@@ -12,12 +12,6 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
