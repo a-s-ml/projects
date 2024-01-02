4dfb37542433bea1b616c4a5c51e075c71621796
asml <p80@80q.ru>
2024-01-02 19:54:07 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -35,18 +35,12 @@ export default function ModalPage({}: ModalProps) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-[var(--tg-theme-bg-color)] px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div>
-                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                    <CheckIcon
-                      className="h-6 w-6 text-green-600"
-                      aria-hidden="true"
-                    />
-                  </div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title
                       as="h3"
-                      className="text-base font-semibold leading-6 text-gray-900"
+                      className="text-base font-semibold leading-6 text-[var(--tg-theme-text-color)]"
                     >
                       {modalData}
                     </Dialog.Title>
