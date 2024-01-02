e4770f7157ad4afa4469a877acce0fe951c8aa9f
asml <p80@80q.ru>
2024-01-02 20:00:18 +0300

    +

--- viktorinaonlinebot/src/components/SettingsGroupForm.tsx
+++ viktorinaonlinebot/src/components/SettingsGroupForm.tsx
@@ -1,22 +1,11 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-
 export default function SettingsGroupForm() {
   return (
     <form>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            This information will be displayed publicly so be careful what you share.
-          </p>
-        </div>
-
-        <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
-
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-
             <div className="sm:col-span-3">
               <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                 Country
@@ -34,22 +23,6 @@ export default function SettingsGroupForm() {
                 </select>
               </div>
             </div>
-
-            <div className="sm:col-span-2 sm:col-start-1">
-              <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
-                City
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="city"
-                  id="city"
-                  autoComplete="address-level2"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
           </div>
         </div>
 
