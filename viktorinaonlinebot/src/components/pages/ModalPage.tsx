c35462a78f4b46a42ef873cc59df722aefafad9d
asml <p80@80q.ru>
2024-01-03 11:25:26 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -18,13 +18,21 @@ export default function ModalPage({}: ModalProps) {
 
   return (
     <Transition.Root show={modal} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-20"
-        onClose={() => dispatch(showModal(false))}
-      >
+      <Dialog as="div" className="relative z-10" onClose={() => dispatch(showModal(false))}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center text-center sm:items-center sm:p-0 w-screen">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -34,19 +42,33 @@ export default function ModalPage({}: ModalProps) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-y-auto rounded-lg bg-[var(--tg-theme-bg-color)] px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-[var(--tg-theme-bg-color)] px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-base font-semibold leading-6 text-[var(--tg-theme-text-color)]"
-                    >
-                      {modalData}
+                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-[var(--tg-theme-text-color)]">
+                    {modalData}
                     </Dialog.Title>
                     <div className="mt-2">
-                      <SettingsGroupForm />
+
+                    <SettingsGroupForm />
                     </div>
                   </div>
+                </div>
+                <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                    onClick={() => dispatch(showModal(false))}
+                  >
+                    Deactivate
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                    onClick={() => dispatch(showModal(false))}
+                  >
+                    Cancel
+                  </button>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
