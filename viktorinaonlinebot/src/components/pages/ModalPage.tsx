e2a52d3e50348f5e09bb5ddc9177d8bec2a03c01
asml <p80@80q.ru>
2024-01-03 11:27:06 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -18,19 +18,11 @@ export default function ModalPage({}: ModalProps) {
 
   return (
     <Transition.Root show={modal} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={() => dispatch(showModal(false))}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => dispatch(showModal(false))}
+      >
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
@@ -45,12 +37,14 @@ export default function ModalPage({}: ModalProps) {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-[var(--tg-theme-bg-color)] px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div>
                   <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-[var(--tg-theme-text-color)]">
-                    {modalData}
+                    <Dialog.Title
+                      as="h3"
+                      className="text-base font-semibold leading-6 text-[var(--tg-theme-text-color)]"
+                    >
+                      {modalData}
                     </Dialog.Title>
                     <div className="mt-2">
-
-                    <SettingsGroupForm />
+                      <SettingsGroupForm />
                     </div>
                   </div>
                 </div>
