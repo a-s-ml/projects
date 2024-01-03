6d0f1d4db189c6b3c510d2bcef1beeef14b08520
asml <p80@80q.ru>
2024-01-03 11:01:31 +0300

    +

--- viktorinaonlinebot/src/components/SettingsGroupForm.tsx
+++ viktorinaonlinebot/src/components/SettingsGroupForm.tsx
@@ -3,11 +3,15 @@ export default function SettingsGroupForm() {
     <form>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Personal Information
+          </h2>
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="country"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Country
               </label>
               <div className="mt-2">
@@ -24,17 +28,43 @@ export default function SettingsGroupForm() {
               </div>
             </div>
           </div>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <div className="relative mb-6">
+                <label htmlFor="labels-range-input" className="sr-only">
+                  Labels range
+                </label>
+                <input
+                  id="labels-range-input"
+                  type="range"
+                  value="1000"
+                  min="100"
+                  max="1500"
+                  className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                />
+                <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">
+                  Min ($100)
+                </span>
+                <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">
+                  $500
+                </span>
+                <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-2/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">
+                  $1000
+                </span>
+                <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">
+                  Max ($1500)
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what else you want to hear about.
-          </p>
-
           <div className="mt-10 space-y-10">
             <fieldset>
-              <legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                By Email
+              </legend>
               <div className="mt-6 space-y-6">
                 <div className="relative flex gap-x-3">
                   <div className="flex h-6 items-center">
@@ -46,10 +76,15 @@ export default function SettingsGroupForm() {
                     />
                   </div>
                   <div className="text-sm leading-6">
-                    <label htmlFor="comments" className="font-medium text-gray-900">
+                    <label
+                      htmlFor="comments"
+                      className="font-medium text-gray-900"
+                    >
                       Comments
                     </label>
-                    <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
+                    <p className="text-gray-500">
+                      Get notified when someones posts a comment on a posting.
+                    </p>
                   </div>
                 </div>
                 <div className="relative flex gap-x-3">
@@ -62,10 +97,15 @@ export default function SettingsGroupForm() {
                     />
                   </div>
                   <div className="text-sm leading-6">
-                    <label htmlFor="candidates" className="font-medium text-gray-900">
+                    <label
+                      htmlFor="candidates"
+                      className="font-medium text-gray-900"
+                    >
                       Candidates
                     </label>
-                    <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
+                    <p className="text-gray-500">
+                      Get notified when a candidate applies for a job.
+                    </p>
                   </div>
                 </div>
                 <div className="relative flex gap-x-3">
@@ -78,17 +118,26 @@ export default function SettingsGroupForm() {
                     />
                   </div>
                   <div className="text-sm leading-6">
-                    <label htmlFor="offers" className="font-medium text-gray-900">
+                    <label
+                      htmlFor="offers"
+                      className="font-medium text-gray-900"
+                    >
                       Offers
                     </label>
-                    <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
+                    <p className="text-gray-500">
+                      Get notified when a candidate accepts or rejects an offer.
+                    </p>
                   </div>
                 </div>
               </div>
             </fieldset>
             <fieldset>
-              <legend className="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>
-              <p className="mt-1 text-sm leading-6 text-gray-600">These are delivered via SMS to your mobile phone.</p>
+              <legend className="text-sm font-semibold leading-6 text-gray-900">
+                Push Notifications
+              </legend>
+              <p className="mt-1 text-sm leading-6 text-gray-600">
+                These are delivered via SMS to your mobile phone.
+              </p>
               <div className="mt-6 space-y-6">
                 <div className="flex items-center gap-x-3">
                   <input
@@ -97,7 +146,10 @@ export default function SettingsGroupForm() {
                     type="radio"
                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                   />
-                  <label htmlFor="push-everything" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="push-everything"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     Everything
                   </label>
                 </div>
@@ -108,7 +160,10 @@ export default function SettingsGroupForm() {
                     type="radio"
                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                   />
-                  <label htmlFor="push-email" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="push-email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     Same as email
                   </label>
                 </div>
@@ -119,7 +174,10 @@ export default function SettingsGroupForm() {
                     type="radio"
                     className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                   />
-                  <label htmlFor="push-nothing" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="push-nothing"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     No push notifications
                   </label>
                 </div>
@@ -130,7 +188,10 @@ export default function SettingsGroupForm() {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
           Cancel
         </button>
         <button
@@ -141,5 +202,5 @@ export default function SettingsGroupForm() {
         </button>
       </div>
     </form>
-  )
+  );
 }
