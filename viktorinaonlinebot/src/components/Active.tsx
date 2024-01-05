7dd7e3093f8fd08bd3523dd4127f4e9146d90b77
asml <p80@80q.ru>
2024-01-05 17:26:35 +0300

    +

--- viktorinaonlinebot/src/components/Active.tsx
+++ viktorinaonlinebot/src/components/Active.tsx
@@ -2,8 +2,8 @@ export default function Active() {
   return (
     <div className="group relative flex items-start space-x-3">
       <div className="min-w-0 flex">
-        <span className="text-xs leading-5 text-[var(--tg-theme-text-color)]">Статус:</span>
         <div className="mt-1 flex items-center gap-x-1.5">
+        <span className="text-xs leading-5 text-[var(--tg-theme-text-color)]">Статус:</span>
           <div className="flex-none rounded-full bg-emerald-500/20 p-1">
             <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
           </div>
