21e75cb5f9e4ee06192c2dee02cb085a8721e471
asml <p80@80q.ru>
2024-01-05 17:24:45 +0300

    +

--- viktorinaonlinebot/src/components/Active.tsx
+++ viktorinaonlinebot/src/components/Active.tsx
@@ -2,7 +2,7 @@ export default function Active() {
   return (
     <div className="group relative flex items-start space-x-3">
       <div className="min-w-0 flex">
-        Статус:
+        <span className="text-xs leading-5 text-[var(--tg-theme-text-color)]">Статус:</span>
         <div className="mt-1 flex items-center gap-x-1.5">
           <div className="flex-none rounded-full bg-emerald-500/20 p-1">
             <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
--- viktorinaonlinebot/src/components/TypeQuestion.tsx
+++ viktorinaonlinebot/src/components/TypeQuestion.tsx
@@ -5,6 +5,7 @@ export default function TypeQuestion({questionType} :TypeQuestionProps) {
   return (
     <div className="group relative flex items-start space-x-3">
       <div className="min-w-0 flex">
+        <span className="text-xs leading-5 text-[var(--tg-theme-text-color)]">Тип вопроса:</span>
         <div className="mt-1 flex items-center gap-x-1.5">
           <p className="text-xs leading-5 text-emerald-500">{questionType}</p>
         </div>
