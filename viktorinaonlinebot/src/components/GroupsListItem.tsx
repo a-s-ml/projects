8b55f48faa32a1b14e86f22f7bed26ce24c96349
asml <p80@80q.ru>
2024-01-08 22:21:31 +0300

    +

--- viktorinaonlinebot/src/components/GroupsListItem.tsx
+++ viktorinaonlinebot/src/components/GroupsListItem.tsx
@@ -1,4 +1,4 @@
-import { Cog8ToothIcon } from "@heroicons/react/24/outline";
+import { Cog8ToothIcon, EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 import {
   useGetInfoActiveGroupsQuery,
   useGetInfoGroupsQuery,
@@ -10,6 +10,8 @@ import Active from "./Active";
 import NotActive from "./notActive";
 import TypeQuestion from "./TypeQuestion";
 import TimeQuestion from "./TimeQuestion";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 interface GroupsListItemProps {
   group: number;
@@ -35,37 +37,68 @@ export default function GroupsListItem({
     <>
       {errorGroupInfo && <li>error</li>}
       {dataGroupInfo && (
-          <li>
-          <div className="relative pb-8">
-              <span className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+        <li className="overflow-hidden rounded-xl border border-gray-200">
+        <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
+          <img
 
-            <div className="relative flex space-x-3">
-              <div>
-                <span>
-                {dataGroupInfo.photo?.small_file_id && (
-                  <GroupAvatar id={dataGroupInfo.photo?.small_file_id} />
-                )}
-                {!dataGroupInfo.photo?.small_file_id && (
-                  <img
-                    className="inline-block h-10 w-14 rounded-full"
-                    src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
-                  />
-                )}
-                </span>
-              </div>
-              <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                <div>
-                  <p className="text-sm text-gray-500">
-                  {dataGroupInfo.title}
-                  </p>
-                </div>
-                <div className="whitespace-nowrap text-right text-sm text-gray-500">
-                @{dataGroupInfo.username}
-                </div>
-              </div>
-            </div>
+            className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
+          />
+          <div className="text-sm font-medium leading-6 text-gray-900">vzzvzcxv</div>
+          <Menu as="div" className="relative ml-auto">
+            <Menu.Button className="-m-2.5 block p-2.5 text-gray-400 hover:text-gray-500">
+              <span className="sr-only">Open options</span>
+              <EllipsisHorizontalIcon className="h-5 w-5" aria-hidden="true" />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                <Menu.Item>
+
+                    <a
+                      href="#"
+                      className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                      View<span className="sr-only">zxczvxcvxc</span>
+                    </a>
+
+                </Menu.Item>
+                <Menu.Item>
+                    <a
+                      href="#"
+                      className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                      Edit<span className="sr-only">, sdfgsfdgsdgf</span>
+                    </a>
+
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+        <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+          <div className="flex justify-between gap-x-4 py-3">
+            <dt className="text-gray-500">Last invoice</dt>
+            <dd className="text-gray-700">
+              <time dateTime='ddfbsz'>bzvzczvc</time>
+            </dd>
           </div>
-        </li>
+          <div className="flex justify-between gap-x-4 py-3">
+            <dt className="text-gray-500">Amount</dt>
+            <dd className="flex items-start gap-x-2">
+              <div className="font-medium text-gray-900">zzcvcxv</div>
+              <div
+                className= 'rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset'>
+                sfdgsfdgg
+              </div>
+            </dd>
+          </div>
+        </dl>
+      </li>
       )}
     </>
   );
