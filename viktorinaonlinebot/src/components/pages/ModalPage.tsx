ca5c947e960695ce79ddc5ecbe5fd07f8fa8bbbe
asml <p80@80q.ru>
2024-01-10 13:50:00 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -25,7 +25,7 @@ export default function ModalPage({}: ModalProps) {
           as="div"
           className="relative z-10"
           initialFocus={cancelButtonRef}
-          onClose={() => dispatch(showModal(false))}
+          onClose={() => dispatch(showModal(true))}
         >
           <Transition.Child
             as={Fragment}
@@ -36,7 +36,7 @@ export default function ModalPage({}: ModalProps) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-200 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -50,18 +50,18 @@ export default function ModalPage({}: ModalProps) {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-[var(--tg-theme-bg-color)] px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-[var(--tg-theme-bg-color)] sm:mx-0 sm:h-10 sm:w-10">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                       <ExclamationTriangleIcon
-                        className="h-6 w-6 text-[var(--tg-theme-accent-text-color)]"
+                        className="h-6 w-6 text-red-600"
                         aria-hidden="true"
                       />
                     </div>
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-base font-semibold leading-6 text-[var(--tg-theme-accent-text-color)]"
+                        className="text-base font-semibold leading-6 text-gray-900"
                       >
                         {modalData}
                       </Dialog.Title>
