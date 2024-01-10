c5007913bca214045a7022de6a246915d021c996
asml <p80@80q.ru>
2024-01-10 16:56:59 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -20,36 +20,33 @@ export default function ModalPage({}: ModalProps) {
   const cancelButtonRef = useRef(null);
   return (
     <>
-      <Transition.Root show={modal} as={Fragment}>
+      <Transition.Root
+        show={modal}
+        as={Fragment}
+        enter="ease-out duration-800"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-800"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
         <Dialog
           as="div"
           className="relative z-10"
           initialFocus={cancelButtonRef}
           onClose={() => dispatch(showModal(true))}
         >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-800"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-800"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Panel className="relative inset-0 text-center transform container w-screen h-full p-6 bg-[var(--tg-theme-bg-color)] transition-all">
-              <Dialog.Title
-                as="h3"
-                className="text-base font-semibold text-[var(--tg-theme-accent-text-color)]"
-              >
-                {modalData}
-              </Dialog.Title>
-              <div className="mt-2">
-                <p className="text-sm text-[var(--tg-theme-accent-text-color)]">
-                  <SettingsGroupForm />
-                </p>
-              </div>
-            </Dialog.Panel>
-          </Transition.Child>
+          <Dialog.Panel className="relative inset-0 text-center transform container w-screen h-full p-6 bg-[var(--tg-theme-bg-color)] transition-all">
+            <Dialog.Title
+              as="h3"
+              className="text-[var(--tg-theme-accent-text-color)]"
+            >
+              {modalData}
+            </Dialog.Title>
+            <p className="text-sm text-[var(--tg-theme-accent-text-color)]">
+              <SettingsGroupForm />
+            </p>
+          </Dialog.Panel>
         </Dialog>
       </Transition.Root>
     </>
