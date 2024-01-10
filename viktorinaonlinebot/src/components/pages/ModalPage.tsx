b4d38c7306ba431da39cf64d370dad669ba9f559
asml <p80@80q.ru>
2024-01-10 17:57:45 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -34,10 +34,10 @@ export default function ModalPage({}: ModalProps) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity h-full w-screen" />
           </Transition.Child>
 
-          <div className="fixed inset-0 z-10 overflow-y-auto h-full w-screen">
+          <div className="fixed inset-0 z-10 overflow-y-auto ">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
