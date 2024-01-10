9fa40e9053a3573add7b46d62e6b81cd0df2d0f6
asml <p80@80q.ru>
2024-01-10 23:29:32 +0300

    +

--- viktorinaonlinebot/src/components/SettingsGroupForm.tsx
+++ viktorinaonlinebot/src/components/SettingsGroupForm.tsx
@@ -39,64 +39,76 @@ export default function SettingsGroupForm({ group }: SettingsGroupProps) {
           <h3 className="text-sm font-medium text-[var(--tg-theme-text-color)]">
             Настройки викторины в группе "{dataGroupInfo.title}"
           </h3>
-          <input
-            onChange={() => selectionChanged()}
-            type="range"
-            className="transparent h-1.5 w-full cursor-pointer appearance-none rounded-lg border-transparent bg-neutral-200"
-            min="0"
-            max="5"
-            step="0.5"
-          />
-          <div className="p-4">
-            <h3 className="text-sm font-medium text-[var(--tg-theme-text-color)]">
-              Вид вопросов
-            </h3>
-            <div className="flex items-center justify-between">
-              {/* <h2 className="text-sm font-medium leading-6 text-gray-900">
+          <ul
+            role="list"
+            className="mt-4 divide-y divide-[var(--tg-theme-hint-color)]"
+          >
+            <li className="py-4 px-0">
+              <h3 className="text-sm font-medium text-left text-[var(--tg-theme-text-color)]">
+                Вид вопросов
+              </h3>
+              <div className="flex items-center justify-between">
+                {/* <h2 className="text-sm font-medium leading-6 text-gray-900">
                 Вид
               </h2> */}
-              <span
-                onClick={() =>
-                  tg.openTelegramLink("https://t.me/ViktorinaOnlineChannel")
-                }
-                className="text-sm font-medium leading-6 text-[var(--tg-theme-accent-text-color)]"
-              >
-                Посмотреть доступный вид вопросов
-              </span>
-            </div>
-
-            <RadioGroup value={type} onChange={setType} className="mt-2">
-              <RadioGroup.Label className="sr-only">
-                Choose a memory option
-              </RadioGroup.Label>
-              <div className="grid grid-cols-3 gap-3 sm:grid-cols-6">
-                {dataType &&
-                  dataType.map((option) => (
-                    <RadioGroup.Option
-                      key={option.id}
-                      value={option}
-                      className={({ active, checked }) =>
-                        classNames(
-                          option.active
-                            ? "cursor-pointer focus:outline-none"
-                            : "cursor-not-allowed opacity-25",
-                          active ? "ring-2 ring-indigo-600 ring-offset-2" : "",
-                          checked
-                            ? "bg-indigo-600 text-white hover:bg-indigo-500"
-                            : "ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50",
-                          "flex items-center justify-center rounded-md py-3 px-3 text-sm font-semibold uppercase sm:flex-1"
-                        )
-                      }
-                      disabled={!option.active}
-                    >
-                      <RadioGroup.Label as="span">
-                        {option.name}
-                      </RadioGroup.Label>
-                    </RadioGroup.Option>
-                  ))}
+                <span
+                  onClick={() =>
+                    tg.openTelegramLink("https://t.me/ViktorinaOnlineChannel")
+                  }
+                  className="text-sm font-medium leading-6 text-[var(--tg-theme-accent-text-color)]"
+                >
+                  Посмотреть доступный вид вопросов
+                </span>
               </div>
-            </RadioGroup>
-          </div>
+
+              <RadioGroup value={type} onChange={setType} className="mt-2">
+                <RadioGroup.Label className="sr-only">
+                  Choose a memory option
+                </RadioGroup.Label>
+                <div className="grid grid-cols-3 gap-3 sm:grid-cols-6">
+                  {dataType &&
+                    dataType.map((option) => (
+                      <RadioGroup.Option
+                        key={option.id}
+                        value={option}
+                        className={({ active, checked }) =>
+                          classNames(
+                            option.active
+                              ? "cursor-pointer focus:outline-none"
+                              : "cursor-not-allowed opacity-25",
+                            active
+                              ? "ring-2 ring-indigo-600 ring-offset-2"
+                              : "",
+                            checked
+                              ? "bg-indigo-600 text-white hover:bg-indigo-500"
+                              : "ring-1 ring-inset ring-gray-300 bg-white text-gray-900 hover:bg-gray-50",
+                            "flex items-center justify-center rounded-md py-3 px-3 text-sm font-semibold uppercase sm:flex-1"
+                          )
+                        }
+                        disabled={!option.active}
+                      >
+                        <RadioGroup.Label as="span">
+                          {option.name}
+                        </RadioGroup.Label>
+                      </RadioGroup.Option>
+                    ))}
+                </div>
+              </RadioGroup>
+            </li>
+            <li className="py-4 px-0">
+              <h3 className="text-sm font-medium text-left text-[var(--tg-theme-text-color)]">
+                Период публикаций
+              </h3>
+              <input
+                onChange={() => selectionChanged()}
+                type="range"
+                className="transparent h-1.5 w-full cursor-pointer appearance-none rounded-lg border-transparent bg-neutral-200"
+                min="0"
+                max="5"
+                step="0.5"
+              />
+            </li>
+          </ul>
         </form>
       )}
       {errorGroupInfo && <ErrorLoad />}
