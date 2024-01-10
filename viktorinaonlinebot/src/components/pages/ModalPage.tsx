ad3111d7ed8ed5efbf0560fbbeb5960f315fbc86
asml <p80@80q.ru>
2024-01-10 13:57:53 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -34,12 +34,12 @@ export default function ModalPage({}: ModalProps) {
           <div className="sm:flex sm:items-start">
             <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
               <ExclamationTriangleIcon
-                className="h-6 w-6 text-red-600"
+                className="h-6 w-6 text-red-600" 
                 aria-hidden="true"
               />
             </div>
             <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-              <Dialog.Title
+              <Dialog.Title 
                 as="h3"
                 className="text-base font-semibold leading-6 text-gray-900"
               >
