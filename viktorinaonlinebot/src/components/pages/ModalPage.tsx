3fce9e32247e47a344fbbfe37c136ba8b5cd8d3a
asml <p80@80q.ru>
2024-01-10 17:54:52 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -37,7 +37,7 @@ export default function ModalPage({}: ModalProps) {
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
-          <div className="fixed inset-0 z-10 overflow-y-auto h-screen w-96 ">
+          <div className="fixed inset-0 z-10 overflow-y-auto h-full w-screen">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -48,20 +48,17 @@ export default function ModalPage({}: ModalProps) {
               leaveTo="opacity-0 translate-y-4"
             >
               <Dialog.Panel className="relative transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all">
-                <div className="flex items-start">
-
-                  <div className="ml-4 mt-0 text-left">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-base font-semibold leading-6 text-gray-900"
-                    >
-                      {modalData}
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        <SettingsGroupForm />
-                      </p>
-                    </div>
+                <div className="ml-4 mt-0 text-left">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-base font-semibold leading-6 text-gray-900"
+                  >
+                    {modalData}
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      <SettingsGroupForm />
+                    </p>
                   </div>
                 </div>
               </Dialog.Panel>
