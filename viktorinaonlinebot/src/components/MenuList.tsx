83b5cd9a0f46b9faf8117f418ca5464f6a84f68c
asml <p80@80q.ru>
2024-01-10 21:19:42 +0300

    +

--- viktorinaonlinebot/src/components/MenuList.tsx
+++ viktorinaonlinebot/src/components/MenuList.tsx
@@ -79,7 +79,7 @@ export default function MenuList({ item, progress }: MenuItemsProps) {
             </>
           )}
         </div>
-        <div className="flex-shrink-0 self-center">
+        <div className="flex-shrink-0 self-center"> 
           <ChevronRightIcon
             className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
             aria-hidden="true"
