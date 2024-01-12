11f1374a0047ccac05d0564213d760e6ce748279
asml <p80@80q.ru>
2024-01-12 21:32:00 +0300

    +

--- viktorinaonlinebot/src/components/addNewGroup.tsx
+++ viktorinaonlinebot/src/components/addNewGroup.tsx
@@ -2,21 +2,19 @@ import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 export default function AddNewGroup() {
   return (
-    <ul role="list" className="divide-y divide-[var(--tg-theme-hint-color)]">
-      <li className="py-4 px-0 ">
-        <div className="group relative flex items-center justify-between ">
-          <div className="text-sm font-medium text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]">
-            <b>
-              <span className="absolute inset-0" aria-hidden="true" />
-              Добавить группу/канал
-            </b>
-          </div>
+    <>
+      <div className="group relative flex items-center justify-between ">
+        <div className="text-sm font-medium text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]">
+          <b>
+            <span className="absolute inset-0" aria-hidden="true" />
+            Добавить группу/канал
+          </b>
         </div>
-        <PlusCircleIcon
-          className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
-          aria-hidden="true"
-        />
-      </li>
-    </ul>
+      </div>
+      <PlusCircleIcon
+        className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
+        aria-hidden="true"
+      />
+    </>
   );
 }
--- viktorinaonlinebot/src/components/pages/Footer.tsx
+++ viktorinaonlinebot/src/components/pages/Footer.tsx
@@ -4,8 +4,8 @@ interface FooterProps {
 
 export function Footer({ children }: FooterProps) {
   return (
-    <footer className="absolute inset-x-0 bottom-0 z-50 bg-[var(--tg-theme-bg-color)]">
-      <div className="bg-gradient-to-t from-inherit to-transparent pt-8 w-screen"></div>
+    <footer className="absolute inset-x-0 bottom-0 z-50">
+      <div className="bg-gradient-to-t from-[var(--tg-theme-bg-color)] to-transparent pt-8 w-screen"></div>
       <div className="flex items-center justify-between p-6 lg:px-8">
         {children}
       </div>
