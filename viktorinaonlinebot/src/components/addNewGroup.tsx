827ba12bab5602bc3adde7ee9948fffd38874c07
asml <p80@80q.ru>
2024-01-12 23:21:24 +0300

    +

--- viktorinaonlinebot/src/components/addNewGroup.tsx
+++ viktorinaonlinebot/src/components/addNewGroup.tsx
@@ -7,13 +7,11 @@ export default function AddNewGroup() {
       className="mt-4 divide-y divide-[var(--tg-theme-hint-color)]"
     >
       <li className="addgroup py-4 px-0">
-        <div className="flex-shrink-0 self-center">
-          <div className="text-sm font-medium text-[var(--tg-theme-hint-color)] addgroup-hover:text-[var(--tg-theme-text-color)]">
-            <b>
-              <span className="absolute inset-0" aria-hidden="true" />
-              Добавить группу/канал
-            </b>
-          </div>
+        <div className="text-sm font-medium text-[var(--tg-theme-hint-color)] addgroup-hover:text-[var(--tg-theme-text-color)]">
+          <b>
+            <span className="absolute inset-0" aria-hidden="true" />
+            Добавить группу/канал
+          </b>
         </div>
       </li>
     </ul>
