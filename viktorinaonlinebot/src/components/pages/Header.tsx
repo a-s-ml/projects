9aa0fd30c6a54cb68be758ff1dce0d9b8ee48d52
asml <p80@80q.ru>
2024-01-12 22:41:34 +0300

    +

--- viktorinaonlinebot/src/components/pages/Header.tsx
+++ viktorinaonlinebot/src/components/pages/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 export function Header({ children }: HeaderProps) {
   return (
-    <header className="absolute inset-x-0 top-0 z-50 p-8 overflow-auto">
+    <header className="fixed top-0 z-50 p-8 overflow-auto">
       <div className="flex items-center justify-between">{children}</div>
     </header>
   );
