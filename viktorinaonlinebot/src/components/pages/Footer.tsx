9aa3111583abe8f521e3f0e5c883ee09ebf5533b
asml <p80@80q.ru>
2024-01-12 22:34:11 +0300

    +

--- viktorinaonlinebot/src/components/pages/Footer.tsx
+++ viktorinaonlinebot/src/components/pages/Footer.tsx
@@ -4,9 +4,9 @@ interface FooterProps {
 
 export function Footer({ children }: FooterProps) {
   return (
-    <footer className="absolute inset-x-0 bottom-0 z-50 px-8">
+    <footer className="absolute inset-x-0 bottom-0 z-50">
       <div className="bg-gradient-to-t from-[var(--tg-theme-bg-color)] to-transparent pt-16 w-screen"></div>
-      <div className="flex items-center justify-between py-2 bg-[var(--tg-theme-bg-color)]">
+      <div className="flex items-center justify-between px-8 py-2 bg-[var(--tg-theme-bg-color)]">
         {children}
       </div>
     </footer>
