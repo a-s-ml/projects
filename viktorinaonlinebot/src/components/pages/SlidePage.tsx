03bd871b8019e7b1bdf84a4f2e58a74c6b5d1fce
asml <p80@80q.ru>
2024-01-13 01:24:34 +0300

    +

--- viktorinaonlinebot/src/components/pages/SlidePage.tsx
+++ viktorinaonlinebot/src/components/pages/SlidePage.tsx
@@ -36,7 +36,6 @@ export default function SlidePage({ chat }: SlideItemsProps) {
           onClose={() => dispatch(showSlide(true))}
         >
           <div className="fixed inset-y-0 right-0 flex max-w-full">
-            <Header>{slideData === "groups" && <AddNewGroup />}</Header>
             <Transition.Child
               as={Fragment}
               enter="transform transition ease-in-out duration-500"
@@ -46,6 +45,7 @@ export default function SlidePage({ chat }: SlideItemsProps) {
               leaveFrom="translate-x-0 opacity-100"
               leaveTo="translate-x-full opacity-5"
             >
+              <Header>{slideData === "groups" && <AddNewGroup />}</Header>
               <Dialog.Panel className="pointer-events-auto relative w-screen">
                 <div className="h-full overflow-y-auto bg-[var(--tg-theme-bg-color)] p-8">
                   {slideData === "groups" && <GroupsList chat={chat} />}
