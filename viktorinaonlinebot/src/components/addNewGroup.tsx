df66434e28553818e81237b0631fa2cd0c3e5587
asml <p80@80q.ru>
2024-01-12 23:15:13 +0300

    +

--- viktorinaonlinebot/src/components/addNewGroup.tsx
+++ viktorinaonlinebot/src/components/addNewGroup.tsx
@@ -7,6 +7,12 @@ export default function AddNewGroup() {
       className="mt-4 divide-y divide-[var(--tg-theme-hint-color)]"
     >
       <li className="py-4 px-0">
+        <div className="flex-shrink-0 self-center">
+          <PlusCircleIcon
+            className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
+            aria-hidden="true"
+          />
+        </div>
         <div className="group relative flex items-start space-x-3">
           <div className="min-w-0 flex-1">
             <div className="text-sm font-medium text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]">
@@ -15,12 +21,6 @@ export default function AddNewGroup() {
                 Добавить группу/канал
               </b>
             </div>
-          </div>
-          <div className="flex-shrink-0 self-center"> 
-            <PlusCircleIcon
-              className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
-              aria-hidden="true"
-            />
           </div>
         </div>
       </li>
