01fbfdd34e00dfaa099ff03215e6b874d36d5214
asml <p80@80q.ru>
2024-01-12 19:32:54 +0300

    +

--- viktorinaonlinebot/src/components/pages/ModalPage.tsx
+++ viktorinaonlinebot/src/components/pages/ModalPage.tsx
@@ -58,15 +58,7 @@ export default function ModalPage() {
                   <Dialog.Panel className="pointer-events-auto relative w-screen">
                     <div className="h-full overflow-y-auto bg-[var(--tg-theme-bg-color)] p-8">
                       {/* <SettingsGroupForm group={modalData}/> */}
-                      <ExPage4 />
-
-                      <ExPage6 />
                       <ExPage7 />
-                      <ExPage8 />
-                      <ExPage9 />
-                      <ExPage10 />
-                      <ExPage11 />
-                      <ExPage12 />
                     </div>
                   </Dialog.Panel>
                 </Transition.Child>
