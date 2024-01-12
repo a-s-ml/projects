e8039f50044355abdb75d255bac0dfc8a365861b
asml <p80@80q.ru>
2024-01-12 23:16:09 +0300

    +

--- viktorinaonlinebot/src/components/addNewGroup.tsx
+++ viktorinaonlinebot/src/components/addNewGroup.tsx
@@ -7,14 +7,14 @@ export default function AddNewGroup() {
       className="mt-4 divide-y divide-[var(--tg-theme-hint-color)]"
     >
       <li className="py-4 px-0">
-        <div className="flex-shrink-0 self-center">
-          <PlusCircleIcon
-            className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
-            aria-hidden="true"
-          />
-        </div>
         <div className="group relative flex items-start space-x-3">
           <div className="min-w-0 flex-1">
+            <PlusCircleIcon
+              className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
+              aria-hidden="true"
+            />
+          </div>
+          <div className="flex-shrink-0 self-center">
             <div className="text-sm font-medium text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]">
               <b>
                 <span className="absolute inset-0" aria-hidden="true" />
