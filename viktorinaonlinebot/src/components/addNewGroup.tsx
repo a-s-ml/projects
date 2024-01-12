c90b60d958bcc903ad7aaabddf2dc10811876215
asml <p80@80q.ru>
2024-01-12 21:51:09 +0300

    +

--- viktorinaonlinebot/src/components/addNewGroup.tsx
+++ viktorinaonlinebot/src/components/addNewGroup.tsx
@@ -2,12 +2,9 @@ import { PlusCircleIcon } from "@heroicons/react/24/outline";
 
 export default function AddNewGroup() {
   return (
-    <ul
-      role="list"
-      className="block divide-y divide-[var(--tg-theme-hint-color)]"
-    >
-      <li className="py-4 px-0 ">
-        <div className="group relative flex items-start space-x-3">
+    <li className="py-4 px-0">
+      <div className="group relative flex items-start space-x-3">
+        <div className="min-w-0 flex-1">
           <div className="text-sm font-medium text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]">
             <b>
               <span className="absolute inset-0" aria-hidden="true" />
@@ -15,11 +12,13 @@ export default function AddNewGroup() {
             </b>
           </div>
         </div>
-        <PlusCircleIcon
-          className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
-          aria-hidden="true"
-        />
-      </li>
-    </ul>
+        <div className="flex-shrink-0 self-center">
+          <PlusCircleIcon
+            className="h-5 w-5 text-[var(--tg-theme-hint-color)] group-hover:text-[var(--tg-theme-text-color)]"
+            aria-hidden="true"
+          />
+        </div>
+      </div>
+    </li>
   );
 }
