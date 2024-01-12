6f8adbc55c37c47ef2d362af000b1dd89427a6ab
asml <p80@80q.ru>
2024-01-12 21:17:31 +0300

    +

--- viktorinaonlinebot/src/components/pages/Footer.tsx
+++ viktorinaonlinebot/src/components/pages/Footer.tsx
@@ -4,7 +4,7 @@ interface FooterProps {
 
 export function Footer({ children }: FooterProps) {
   return (
-    <header className="absolute inset-x-0 bottom-0 z-50 bg-gradient-to-b from-inherit to-transparent">
+    <header className="absolute inset-x-0 bottom-0 z-50 bg-gradient-to-t from-inherit to-indigo-500">
       <div className="flex items-center justify-between p-6 lg:px-8">
         {children}
       </div>
