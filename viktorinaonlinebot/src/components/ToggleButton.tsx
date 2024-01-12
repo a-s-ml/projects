c24eba208210b45a4b8443a780f7dc8e72cbe8f7
asml <p80@80q.ru>
2024-01-13 00:51:26 +0300

    +

--- viktorinaonlinebot/src/components/ToggleButton.tsx
+++ viktorinaonlinebot/src/components/ToggleButton.tsx
@@ -76,7 +76,7 @@ export default function ToggleButton({ group, state }: ToggleButtonProps) {
         <span
           className={classNames(
             enabled ? "translate-x-5" : "translate-x-0",
-            "pointer-events-none relative inline-block h-4 w-4 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+            "pointer-events-none relative inline-block h-4 w-4 transform rounded-full bg-white shadow-md ring-0 transition duration-200 ease-in-out"
           )}
         >
           <span
