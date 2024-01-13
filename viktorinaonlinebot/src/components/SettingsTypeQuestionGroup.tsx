e11b0b037d7e3a8fece110286736b0e967eb419b
asml <p80@80q.ru>
2024-01-13 20:47:11 +0300

    +

--- viktorinaonlinebot/src/components/SettingsTypeQuestionGroup.tsx
+++ viktorinaonlinebot/src/components/SettingsTypeQuestionGroup.tsx
@@ -1,7 +1,6 @@
 import {
   ChevronDoubleDownIcon,
-  MinusIcon,
-  PlusIcon,
+  ChevronDoubleUpIcon,
 } from "@heroicons/react/24/outline";
 import { IType } from "../models/IType";
 import { Disclosure, RadioGroup } from "@headlessui/react";
@@ -29,43 +28,33 @@ export default function SettingsTypeQuestionGroup({
   }
   return (
     <>
-      <li className="py-4 px-0">
-        <div className="group relative flex items-start space-x-3">
-          <div className="min-w-0 flex-1">
-            <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
-              <b>Вид вопросов</b>
-            </div>
-          </div>
-          <div className="flex-shrink-0 self-center text-right">
-            <ChevronDoubleDownIcon />
-          </div>
-        </div>
-        <div className="flex items-center justify-between">
-          <span
-            onClick={() =>
-              tg.openTelegramLink("https://t.me/ViktorinaOnlineChannel")
-            }
-            className="cursor-pointer text-sm font-medium leading-6 text-[var(--tg-theme-accent-text-color)]"
-          >
-            Посмотреть пример
-          </span>
-        </div>
-      </li>
-      <Disclosure as="div" className="border-t border-gray-200 px-4 py-6">
+      <Disclosure as="div">
         {({ open }) => (
           <>
-            <h3 className="-mx-2 -my-3 flow-root">
-              <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
-                <span className="font-medium text-gray-900">Вид вопросов</span>
-                <span className="ml-6 flex items-center">
-                  {open ? (
-                    <MinusIcon className="h-5 w-5" aria-hidden="true" />
-                  ) : (
-                    <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                  )}
-                </span>
+            <li className="py-4 px-0">
+              <Disclosure.Button className="flex w-full items-center justify-between">
+                <div className="group relative flex items-start space-x-3">
+                  <div className="min-w-0 flex-1">
+                    <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
+                      <b>Вид вопросов</b>
+                    </div>
+                  </div>
+                  <div className="flex-shrink-0 self-center text-right">
+                    {open ? (
+                      <ChevronDoubleUpIcon
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                      />
+                    ) : (
+                      <ChevronDoubleDownIcon
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                      />
+                    )}
+                  </div>
+                </div>
               </Disclosure.Button>
-            </h3>
+            </li>
             <Disclosure.Panel className="pt-6">
               <div className="space-y-6">
                 <RadioGroup value={type} onChange={setType} className="mt-2">
