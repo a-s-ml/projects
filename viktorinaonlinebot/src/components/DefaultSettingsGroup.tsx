25793c26628ff840758ab5c75ed6f7b6e8ecdd84
asml <p80@80q.ru>
2024-01-14 00:19:45 +0300

    +

--- viktorinaonlinebot/src/components/DefaultSettingsGroup.tsx
+++ viktorinaonlinebot/src/components/DefaultSettingsGroup.tsx
@@ -3,18 +3,17 @@ import { ArrowPathIcon } from "@heroicons/react/24/outline";
 export default function DefaultSettingsGroup() {
   const tg = window.Telegram.WebApp;
   return (
-    <div className="group flex items-start px-8 pt-8 pb-4 bg-[var(--tg-theme-bg-color)] space-x-3 cursor-pointer">
-      <div
-        className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
-        onClick={() => tg.MainButton.show()}
-      >
-        <b>Восстановить значения по-умолчанию</b>
-      </div>
-
+    <div
+      className="group flex items-start px-8 pt-8 pb-4 bg-[var(--tg-theme-bg-color)] space-x-3 cursor-pointer"
+      onClick={() => tg.MainButton.show()}
+    >
       <ArrowPathIcon
         className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
         aria-hidden="true"
       />
+      <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
+        <b>Восстановить значения по-умолчанию</b>
+      </div>
     </div>
   );
 }
