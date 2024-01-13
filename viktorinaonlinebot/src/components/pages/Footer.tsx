7839d4cd188279ceb52395a5f843ede6b5b12032
asml <p80@80q.ru>
2024-01-13 18:34:01 +0300

    +

--- viktorinaonlinebot/src/components/pages/Footer.tsx
+++ viktorinaonlinebot/src/components/pages/Footer.tsx
@@ -4,21 +4,18 @@ export function Footer() {
   return (
     <footer className="absolute inset-x-0 bottom-0 z-50">
       <div className="bg-gradient-to-t from-[var(--tg-theme-bg-color)] to-transparent pt-16 w-screen"></div>
-      <div className="flex items-center justify-between px-8 py-2 bg-[var(--tg-theme-bg-color)]">
-        <div className="min-w-0 flex-1">
-          <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
-            <b>
-              <span className="absolute inset-0" aria-hidden="true" />
-              Вопрос/Отзыв/предложение
-            </b>
-          </div>
+      <div className="flex items-start px-8 py-2 bg-[var(--tg-theme-bg-color)]">
+        <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
+          <b>
+            <span className="absolute inset-0" aria-hidden="true" />
+            Вопрос/Отзыв/предложение
+          </b>
         </div>
-        <div className="flex-shrink-0 self-center">
-          <ChatBubbleOvalLeftEllipsisIcon
-            className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
-            aria-hidden="true"
-          />
-        </div>
+
+        <ChatBubbleOvalLeftEllipsisIcon
+          className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
+          aria-hidden="true"
+        />
       </div>
     </footer>
   );
