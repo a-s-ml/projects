fa3729057a58fa47bd95fd4bb2276532477106a2
asml <p80@80q.ru>
2024-01-13 23:36:06 +0300

    +

--- viktorinaonlinebot/src/components/SettingsTypeQuestionGroup.tsx
+++ viktorinaonlinebot/src/components/SettingsTypeQuestionGroup.tsx
@@ -14,7 +14,7 @@ export default function SettingsTypeQuestionGroup({
   dataType,
   type,
   setType,
-  selectionChanged,
+  selectionChanged
 }: SettingsTypeQuestionGroupProps) {
   const tg = window.Telegram.WebApp;
   function classNames(...classes: string[]) {
@@ -28,10 +28,10 @@ export default function SettingsTypeQuestionGroup({
             <li className="py-4 px-0">
               <Disclosure.Button className="group relative flex w-full items-start justify-between space-x-3">
                 <div className="min-w-0 flex-1 text-left">
-                  <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
+                  <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
                     Вид вопросов
                   </div>
-                  <span className="text-xs leading-5 text-[var(--tg-theme-hint-color)]">
+                  <span className="text-xs leading-5 text-[var(--tg-theme-text-color)]">
                     Тукущий вид: {type}
                   </span>
                 </div>
@@ -51,54 +51,37 @@ export default function SettingsTypeQuestionGroup({
               </Disclosure.Button>
             </li>
             <Disclosure.Panel className="pt-6">
-              <div className="space-y-1.5">
-                <RadioGroup value={type} onChange={setType}>
-                  <div className="-space-y-px rounded-md">
+              <div className="space-y-6">
+                <RadioGroup value={type} onChange={setType} className="mt-2">
+                  <RadioGroup.Label className="sr-only">
+                    Choose a memory option
+                  </RadioGroup.Label>
+                  <div className="grid grid-cols-3 gap-3 sm:grid-cols-6">
                     {dataType &&
                       dataType.map((type) => (
                         <RadioGroup.Option
                           key={type.id}
                           value={type.name}
-                          className={({ checked }) =>
+                          onClick={() => selectionChanged()}
+                          className={({ active, checked }) =>
                             classNames(
+                              Boolean(type.active)
+                                ? "cursor-pointer"
+                                : "cursor-not-allowed opacity-25",
+                              active
+                                ? "ring-2 ring-[var(--tg-theme-accent-text-color)] ring-offset-2"
+                                : "",
                               checked
-                                ? "z-10 border-2 border-[var(--tg-theme-text-color)]"
-                                : " border-1 border-[var(--tg-theme-hint-color)]",
-                              "relative flex cursor-pointer border p-4"
+                                ? "bg-[var(--tg-theme-accent-text-color)] text-white"
+                                : "ring-1 ring-inset ring-[var(--tg-theme-hint-color)] bg-white text-black",
+                              "flex items-center justify-center rounded-md py-3 px-3 text-sm font-semibold uppercase"
                             )
                           }
+                          disabled={!Boolean(type.active)}
                         >
-                          {({ active, checked }) => (
-                            <>
-                              <span
-                                className={classNames(
-                                  checked
-                                    ? "bg-[var(--tg-theme-accent-text-color)] border-transparent"
-                                    : "border-[var(--tg-theme-accent-text-color)]",
-                                  Boolean(active)
-                                    ? "ring-2 ring-offset-2 ring-[var(--tg-theme-accent-text-color)]"
-                                    : "",
-                                  "mt-0.5 h-4 w-4 shrink-0 cursor-pointer rounded-full border flex items-center justify-center"
-                                )}
-                                aria-hidden="true"
-                              >
-                                <span className="rounded-full w-1.5 h-1.5" />
-                              </span>
-                              <span className="ml-3 flex flex-col">
-                                <RadioGroup.Label
-                                  as="span"
-                                  className={classNames(
-                                    checked
-                                      ? "text-[var(--tg-theme-text-color)] font-bold" 
-                                      : "text-[var(--tg-theme-hint-color)] font-medium",
-                                    "block text-sm"
-                                  )}
-                                >
-                                  {type.description}
-                                </RadioGroup.Label>
-                              </span>
-                            </>
-                          )}
+                          <RadioGroup.Label as="span">
+                            {type.description}
+                          </RadioGroup.Label>
                         </RadioGroup.Option>
                       ))}
                   </div>
