e48232bb6e6492d095e6bca7be56fcb234915f7d
asml <p80@80q.ru>
2024-01-13 19:00:50 +0300

    +

--- viktorinaonlinebot/src/components/pages/Footer.tsx
+++ viktorinaonlinebot/src/components/pages/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
     <footer className="absolute inset-x-0 bottom-0 z-50">
       <div className="bg-gradient-to-t from-[var(--tg-theme-bg-color)] to-transparent pt-16 w-screen"></div>
       <div
-        className="flex items-start px-8 py-2 bg-[var(--tg-theme-bg-color)] space-x-3"
+        className="group flex items-start px-8 py-2 bg-[var(--tg-theme-bg-color)] space-x-3 cursor-pointer"
         onClick={() => tg.openTelegramLink("https://t.me/a_s_ml")}
       >
         <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
