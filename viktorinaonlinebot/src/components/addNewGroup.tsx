4e3922dd2cc9208a8ac0e177fa38f44db4c02a66
asml <p80@80q.ru>
2024-01-13 19:14:05 +0300

    +

--- viktorinaonlinebot/src/components/addNewGroup.tsx
+++ viktorinaonlinebot/src/components/addNewGroup.tsx
@@ -4,7 +4,7 @@ export default function AddNewGroup() {
   return (
     <>
       <div className="min-w-0 flex-1">
-        <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
+        <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] headerblock-hover:text-[var(--tg-theme-text-color)]">
           <b>
             <span className="absolute inset-0" aria-hidden="true" />
             Добавить группу/канал
@@ -13,7 +13,7 @@ export default function AddNewGroup() {
       </div>
       <div className="flex-shrink-0 self-center">
         <PlusCircleIcon
-          className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
+          className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] headerblock-hover:text-[var(--tg-theme-text-color)]"
           aria-hidden="true"
         />
       </div>
--- viktorinaonlinebot/src/components/pages/Header.tsx
+++ viktorinaonlinebot/src/components/pages/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 export function Header({ children }: HeaderProps) {
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-      <div className="flex items-center justify-between px-8 pt-8 pb-4 bg-[var(--tg-theme-bg-color)]">
+      <div className="headerblock flex items-center justify-between px-8 pt-8 pb-4 bg-[var(--tg-theme-bg-color)]">
         {children}
       </div>
       <div className="bg-gradient-to-b from-[var(--tg-theme-bg-color)] to-transparent pt-12 w-screen"></div>
