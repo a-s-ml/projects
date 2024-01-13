d61d2f99b98e703aa529cd54e2fdff8a97800c18
asml <p80@80q.ru>
2024-01-13 18:47:49 +0300

    +

--- viktorinaonlinebot/src/components/pages/Header.tsx
+++ viktorinaonlinebot/src/components/pages/Header.tsx
@@ -4,8 +4,8 @@ interface HeaderProps {
 
 export function Header({ children }: HeaderProps) {
   return (
-    <header className="absolute inset-x-0 top-0 z-50">
-      <div className="flex items-center justify-between pt-4 px-8 pb-2 bg-[var(--tg-theme-bg-color)]">
+    <header className="absolute inset-x-0 top-0 z-50 pt-4">
+      <div className="flex items-center justify-between px-8 py-2 bg-[var(--tg-theme-bg-color)]">
         {children}
       </div>
       <div className="bg-gradient-to-b from-[var(--tg-theme-bg-color)] to-transparent pt-12 w-screen"></div>
