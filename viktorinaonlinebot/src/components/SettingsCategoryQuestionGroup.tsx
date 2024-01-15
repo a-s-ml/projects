c94af54c2e7581f39908c1f90bf65297337b145a
asml <p80@80q.ru>
2024-01-15 03:28:25 +0300

    +

--- viktorinaonlinebot/src/components/SettingsCategoryQuestionGroup.tsx
+++ viktorinaonlinebot/src/components/SettingsCategoryQuestionGroup.tsx
@@ -25,15 +25,9 @@ export default function SettingsCategoryQuestionGroup({
             <li className="py-4 px-0">
               <Disclosure.Button className="group relative flex w-full items-start justify-between space-x-3">
                 <div className="min-w-0 flex-1 text-left">
-                  <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
+                  <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
                     Категории вопросов
                   </div>
-                  <p className="text-xs leading-5 text-[var(--tg-theme-text-color)]">
-                    Выбрано категорий:
-                  </p>
-                  <p className="text-xs leading-5 text-[var(--tg-theme-text-color)]">
-                    Вопросов в выбранных категориях:
-                  </p>
                 </div>
                 <div className="flex-shrink-0 self-center text-right">
                   {open ? (
--- viktorinaonlinebot/src/components/SettingsTimeQuestionGroup.tsx
+++ viktorinaonlinebot/src/components/SettingsTimeQuestionGroup.tsx
@@ -39,12 +39,9 @@ export default function SettingsTimeQuestionGroup({
             <li className="py-4 px-0">
               <Disclosure.Button className="group relative flex w-full items-start justify-between space-x-3">
                 <div className="min-w-0 flex-1 text-left">
-                  <div className="text-sm font-medium text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]">
+                  <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
                     Период публикаций
                   </div>
-                  <span className="text-xs leading-5 text-[var(--tg-theme-text-color)]">
-                    Тукущий период: {time}
-                  </span>
                 </div>
                 <div className="flex-shrink-0 self-center text-right">
                   {open ? (
