d1dcceddfd2ea5980d5a6456dfb3fc59afc3317e
asml <p80@80q.ru>
2024-01-18 00:15:25 +0300

    +

--- viktorinaonlinebot/src/components/pages/question/NewQuesion.tsx
+++ viktorinaonlinebot/src/components/pages/question/NewQuesion.tsx
@@ -43,28 +43,35 @@ export default function NewQuesion() {
   };
 
   return (
-    <form className="text-center py-24">
-      <div>
-        <label
-          htmlFor="questionText"
-          className="block text-sm font-medium leading-6 text-[var(--tg-theme-text-color)]"
-        >
-          Текст вопроса:
-        </label>
-        <div className="mt-2">
-          <textarea
-            rows={5}
-            name="questionText"
-            id="questionText"
-            className="block w-full bg-[var(--tg-theme-bg-color)] rounded-md border-0 py-1.5 text-[var(--tg-theme-text-color)] shadow-sm ring-1 ring-inset ring-[var(--tg-theme-text-color)] placeholder:text-[var(--tg-theme-hint-color)]"
-            value={text}
-            onChange={onTextChanged}
-          />
+    <>
+      <form className="text-center py-24">
+        <div>
+          <label
+            htmlFor="questionText"
+            className="block text-sm font-medium leading-6 text-[var(--tg-theme-text-color)]"
+          >
+            Текст вопроса:
+          </label>
+          <div className="mt-2">
+            <textarea
+              rows={5}
+              name="questionText"
+              id="questionText"
+              className="block w-full bg-[var(--tg-theme-bg-color)] rounded-md border-0 py-1.5 text-[var(--tg-theme-text-color)] shadow-sm ring-1 ring-inset ring-[var(--tg-theme-text-color)] placeholder:text-[var(--tg-theme-hint-color)]"
+              value={text}
+              onChange={onTextChanged}
+            />
+          </div>
         </div>
-      </div>
 
-      <CategoryList categories={categories.all} cat={category} setCategory={setCategory} />
-      <AnswersList />
-    </form>
+        <CategoryList
+          categories={categories.all}
+          cat={category}
+          setCategory={setCategory}
+        />
+        <AnswersList />
+      </form>
+      <div className="pb-6"></div>
+    </>
   );
 }
