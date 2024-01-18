b2da892d05ebe06d445835e11630e429da3614df
asml <p80@80q.ru>
2024-01-19 02:03:41 +0700

    +

--- viktorinaonlinebot/src/components/Preloader/Preloader.tsx
+++ viktorinaonlinebot/src/components/Preloader/Preloader.tsx
@@ -2,7 +2,7 @@ import styles from './Preloader.module.css';
 
 export default function Preloader() {
     return (
-        <div>
+        <div className='flex items-center justify-center w-full h-20'>
             <span className={styles.loader}/>
         </div>
     )
