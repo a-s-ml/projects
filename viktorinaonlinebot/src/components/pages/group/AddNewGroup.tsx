fdb73468eb4e2f821aa91e0abd1faf2f67e8664b
asml <p80@80q.ru>
2024-01-19 02:39:22 +0700

    +

--- viktorinaonlinebot/src/components/pages/group/AddNewGroup.tsx
+++ viktorinaonlinebot/src/components/pages/group/AddNewGroup.tsx
@@ -1,11 +1,8 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import useLongPress from "../../hooks/useLongPress";
-import { useAppDispatch } from "../../store";
 import SlideDown from "../SlideDown";
 
 export default function AddNewGroup() {
-  const dispatch = useAppDispatch();
-  const { action, handlers } = useLongPress();
   const { action: otherAction, handlers: otherHandlers } = useLongPress();
 
   return (
@@ -13,7 +10,7 @@ export default function AddNewGroup() {
       className="group flex items-start px-8 pt-8 pb-4 bg-[var(--tg-theme-bg-color)] space-x-3 cursor-pointer"
       {...otherHandlers}
     >
-      {action === "longpress" && <SlideDown slide={true} />}
+      {otherAction === "longpress" && <SlideDown slide={true} />}
       <PlusCircleIcon
         className="h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)]"
         aria-hidden="true"
