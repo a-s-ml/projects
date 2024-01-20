13227fea7e985b1550c7399c78d5f4e9ffd091f0
asml <p80@80q.ru>
2024-01-21 01:14:45 +0700

    +

--- viktorinaonlinebot/src/components/pages/group/swipe/swipe.tsx
+++ viktorinaonlinebot/src/components/pages/group/swipe/swipe.tsx
@@ -1,5 +1,4 @@
-import { Dispatch, SetStateAction, useRef } from "react";
-import styled from "styled-components";
+import { useRef } from "react";
 
 interface SwipeToDeleteProps {
   rows: {
@@ -11,78 +10,61 @@ interface SwipeToDeleteProps {
 
 export const SwipeToDelete = ({ rows, onDel }: SwipeToDeleteProps) => {
   return (
-    <Wrapper>
+    <div className="w-9/12 border-solid border-1 overflow-hidden">
       {rows.map((row) => (
         <Item key={row.id}>
-          <p>{row.title}</p>
-          <DeleteButton onClick={() => onDel(row.id)}>Delete</DeleteButton>
+          <p className="flex-1 w-full">{row.title}</p>
+          <div
+            className="bg-red-500 text-left min-w-[55px]"
+            onClick={() => onDel(row.id)}
+          >
+            Delete
+          </div>
         </Item>
       ))}
-    </Wrapper>
+    </div>
   );
 };
 
 export default SwipeToDelete;
-
-const Wrapper = styled.div`
-  width: 80%;
-  border: 1px solid;
-  overflow: hidden;
-`;
-
-const DeleteButton = styled.div`
-  background: red;
-  text-align: left;
-  border: none;
-  min-width: 55px;
-`;
 
 interface ItemProps {
   children: React.ReactNode;
 }
 
 const Item = ({ children }: ItemProps) => {
-  const ref = useRef<HTMLElement>(null);
+  const refOn = useRef<HTMLElement>(null);
   let downX: number;
 
   const onPointerMove = (e: { clientX: number }) => {
     const newX = e.clientX;
-    if (ref.current) {
+    if (refOn.current) {
       if (newX - downX < 50) {
-        ref.current.style.transform = "translate(-55px)";
+        refOn.current.style.transform = "translate(-55px)";
         setTimeout(() => {
-          if (ref.current) ref.current.style.transform = "translate(0px)";
+          if (refOn.current) refOn.current.style.transform = "translate(0px)";
         }, 4000);
-      } else ref.current.style.transform = "translate(0px)";
+      } else refOn.current.style.transform = "translate(0px)";
     }
   };
 
   const onPointerUp = () =>
-    ref.current?.removeEventListener("pointermove", onPointerMove);
+    refOn.current?.removeEventListener("pointermove", onPointerMove);
 
   const onPointerDown = (e: { clientX: number }) => {
     downX = e.clientX;
-    if (ref.current) {
-      ref.current.addEventListener("pointermove", onPointerMove);
+    if (refOn.current) {
+      refOn.current.addEventListener("pointermove", onPointerMove);
     }
   };
 
   return (
-    <ItemWrapper
+    <div
+      className="flex bg-slate-50 my-1 mx-0 transition transform duration-800"
       onPointerDown={onPointerDown}
       onPointerUp={onPointerUp}
     >
       {children}
-    </ItemWrapper>
+    </div>
   );
 };
-
-const ItemWrapper = styled.div`
-  display: flex;
-  background-color: lightgray;
-  margin: 3px 0;
-  transition: transform 800ms;
-  p {
-    flex: 1 0 100%;
-  }
-`;
