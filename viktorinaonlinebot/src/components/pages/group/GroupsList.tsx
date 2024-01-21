b4e23d1091f6d9888720719f8b7146c62090e518
asml <p80@80q.ru>
2024-01-22 04:58:53 +0700

    +

--- viktorinaonlinebot/src/components/pages/group/GroupsList.tsx
+++ viktorinaonlinebot/src/components/pages/group/GroupsList.tsx
@@ -20,7 +20,7 @@ export default function GroupsList({ chat }: GroupsListProps) {
       {errorGroups && <ErrorPage />}
       <ul
         role="list"
-        className="mt-4 divide-y divide-[var(--tg-theme-hint-color)] pt-9"
+        className="mt-4 divide-y divide-[var(--tg-theme-hint-color)] py-9"
       >
         {successGroup &&
           dataGroups.map((group) => (
