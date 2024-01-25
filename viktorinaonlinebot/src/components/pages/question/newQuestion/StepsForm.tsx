ea1802c8f6c6d444aaa65fbf9edc315f01ccef4c
asml <p80@80q.ru>
2024-01-25 19:48:47 +0300

    +

--- viktorinaonlinebot/src/components/pages/question/newQuestion/StepsForm.tsx
+++ viktorinaonlinebot/src/components/pages/question/newQuestion/StepsForm.tsx
@@ -1,9 +1,8 @@
-import { CheckIcon } from "@heroicons/react/24/solid";
-
 const steps = [
-  { id: 0, name: "Текст", href: "#", status: "complete" },
-  { id: 1, name: "Категория", href: "#", status: "current" },
-  { id: 2, name: "Варианты ответа", href: "#", status: "upcoming" },
+    { name: 'Step 1', href: '#', status: 'complete' },
+    { name: 'Step 2', href: '#', status: 'current' },
+    { name: 'Step 3', href: '#', status: 'upcoming' },
+    { name: 'Step 4', href: '#', status: 'upcoming' },
 ];
 
 interface StepsFormProps {
@@ -13,55 +12,33 @@ interface StepsFormProps {
 export default function StepsForm({ stepid }: StepsFormProps) {
   const step = steps[stepid];
   return (
-    <nav aria-label="Progress">
-      <ol
-        role="list"
-        className="divide-y divide-gray-300 rounded-md border border-gray-300 md:flex md:divide-y-0"
-      >
-        <li key={step.name} className="relative md:flex md:flex-1">
-          {step.status === "complete" ? (
-            <a href={step.href} className="group flex w-full items-center">
-              <span className="flex items-center px-6 py-4 text-sm font-medium">
-                <span className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full bg-indigo-600 group-hover:bg-indigo-800">
-                  <CheckIcon
-                    className="h-6 w-6 text-white"
-                    aria-hidden="true"
-                  />
-                </span>
-                <span className="ml-4 text-sm font-medium text-gray-900">
-                  {step.name}
-                </span>
-              </span>
+    <nav className="flex items-center justify-center" aria-label="Progress">
+    <p className="text-sm font-medium">
+      Step {steps.findIndex((step) => step.status === 'current') + 1} of {steps.length}
+    </p>
+    <ol role="list" className="ml-8 flex items-center space-x-5">
+      {steps.map((step) => (
+        <li key={step.name}>
+          {step.status === 'complete' ? (
+            <a href={step.href} className="block h-2.5 w-2.5 rounded-full bg-indigo-600 hover:bg-indigo-900">
+              <span className="sr-only">{step.name}</span>
             </a>
-          ) : step.status === "current" ? (
-            <a
-              href={step.href}
-              className="flex items-center px-6 py-4 text-sm font-medium"
-              aria-current="step"
-            >
-              <span className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-indigo-600">
-                <span className="text-indigo-600">{step.id}</span>
+          ) : step.status === 'current' ? (
+            <a href={step.href} className="relative flex items-center justify-center" aria-current="step">
+              <span className="absolute flex h-5 w-5 p-px" aria-hidden="true">
+                <span className="h-full w-full rounded-full bg-indigo-200" />
               </span>
-              <span className="ml-4 text-sm font-medium text-indigo-600">
-                {step.name}
-              </span>
+              <span className="relative block h-2.5 w-2.5 rounded-full bg-indigo-600" aria-hidden="true" />
+              <span className="sr-only">{step.name}</span>
             </a>
           ) : (
-            <a href={step.href} className="group flex items-center">
-              <span className="flex items-center px-6 py-4 text-sm font-medium">
-                <span className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-full border-2 border-gray-300 group-hover:border-gray-400">
-                  <span className="text-gray-500 group-hover:text-gray-900">
-                    {step.id}
-                  </span>
-                </span>
-                <span className="ml-4 text-sm font-medium text-gray-500 group-hover:text-gray-900">
-                  {step.name}
-                </span>
-              </span>
+            <a href={step.href} className="block h-2.5 w-2.5 rounded-full bg-gray-200 hover:bg-gray-400">
+              <span className="sr-only">{step.name}</span>
             </a>
           )}
         </li>
-      </ol>
-    </nav>
+      ))}
+    </ol>
+  </nav>
   );
 }
