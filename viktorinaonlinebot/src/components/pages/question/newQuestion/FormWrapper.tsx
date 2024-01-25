77d4b25e1869289c1abbb8e0e11391efbeaec14f
asml <p80@80q.ru>
2024-01-25 21:04:04 +0300

    +

--- viktorinaonlinebot/src/components/pages/question/newQuestion/FormWrapper.tsx
+++ viktorinaonlinebot/src/components/pages/question/newQuestion/FormWrapper.tsx
@@ -1,26 +1,20 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 type FormWrapperProps = {
-  title: string
-  children: ReactNode
-}
+  title: string;
+  children: ReactNode;
+};
 
 export function FormWrapper({ title, children }: FormWrapperProps) {
   return (
     <>
-      <h2 style={{ textAlign: "center", margin: 0, marginBottom: "2rem" }}>
+      {children}
+      <h2
+        className="text-[var(--tg-theme-text-color)]"
+        style={{ textAlign: "center", margin: 0, marginBottom: "2rem" }}
+      >
         {title}
       </h2>
-      <div
-        style={{
-          display: "grid",
-          gap: "1rem .5rem",
-          justifyContent: "flex-start",
-          gridTemplateColumns: "auto minmax(auto, 400px)",
-        }}
-      >
-        {children}
-      </div>
     </>
-  )
+  );
 }
