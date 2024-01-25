87fce7d5417f384597cd95c9de361e2059123342
asml <p80@80q.ru>
2024-01-25 19:19:54 +0300

    +

--- viktorinaonlinebot/src/components/pages/question/NewQuesion.tsx
+++ viktorinaonlinebot/src/components/pages/question/NewQuesion.tsx
@@ -1,58 +1,96 @@
-import { useState } from "react";
-import { useAddQuestionMutation } from "../../store/api/question/question.api";
+import { FormEvent, useState } from "react";
+import { UserForm } from "./newQuestion/UserForm";
+import { AddressForm } from "./newQuestion/AddressForm";
+import { AccountForm } from "./newQuestion/AccountForm";
+import { useMultistepForm } from "../../hooks/useNewQuestionFormContext";
 import { useAppSelector } from "../../store";
-import { selectAllCategories } from "../../store/api/category/category.slice";
-import CategoryList from "./newQuestion/CategoryList";
-import AnswersList from "./newQuestion/AnswersList";
-import TexList from "./newQuestion/TexList";
+import { selectSlideUser } from "../../store/api/slide.slice";
 
-export default function NewQuesion() {
-  const [addNewQuestion, { isLoading }] = useAddQuestionMutation();
+type FormData = {
+  firstName: string;
+  lastName: string;
+  age: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  email: string;
+  password: string;
+};
 
-  const [text, setText] = useState("");
-  const [content, setContent] = useState("");
-  const [category, setCategory] = useState(0);
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+};
 
-  const categories = useAppSelector(selectAllCategories);
+function NewQuesion() {
+  const user = useAppSelector(selectSlideUser);
+  const [data, setData] = useState(INITIAL_DATA);
+  function updateFields(fields: Partial<FormData>) {
+    setData((prev) => {
+      return { ...prev, ...fields };
+    });
+  }
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([
+      <UserForm {...data} updateFields={updateFields} />,
+      <AddressForm {...data} updateFields={updateFields} />,
+      <AccountForm {...data} updateFields={updateFields} />,
+    ]);
 
-  const canSave = [text, content, category].every(Boolean) && !isLoading;
-
-  const onSavePostClicked = async () => {
-    if (canSave) {
-      try {
-        await addNewQuestion({
-          text: text,
-          chat: 0n,
-          category: 0,
-          answer1: "",
-          answer2: "",
-          answer3: "",
-          answer4: "",
-          answerright: 0,
-        }).unwrap();
-
-        setText("");
-        setContent("");
-        setCategory(55);
-      } catch (err) {
-        console.error("Failed to save the post", err);
-      }
-    }
-  };
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!isLastStep) return next();
+    alert("Successful Account Creation");
+  }
 
   return (
     <>
-      <form className="text-left pt-14 pb-24">
-        <TexList text={text} />
-        <CategoryList
-          categories={categories.all}
-          cat={category}
-          setCategory={setCategory}
-        />
-        <AnswersList />
-      </form>
-
-      <div className="pb-6"></div>
+      (user === 521884639 && (
+      <div
+        style={{
+          position: "relative",
+          background: "white",
+          border: "1px solid black",
+          padding: "2rem",
+          margin: "1rem",
+          borderRadius: ".5rem",
+          fontFamily: "Arial",
+          maxWidth: "max-content",
+        }}
+      >
+        <form onSubmit={onSubmit}>
+          <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+            {currentStepIndex + 1} / {steps.length}
+          </div>
+          {step}
+          <div
+            style={{
+              marginTop: "1rem",
+              display: "flex",
+              gap: ".5rem",
+              justifyContent: "flex-end",
+            }}
+          >
+            {!isFirstStep && (
+              <button type="button" onClick={back}>
+                Back
+              </button>
+            )}
+            <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
+          </div>
+        </form>
+      </div>
+      ))
     </>
   );
 }
+
+export default NewQuesion;
