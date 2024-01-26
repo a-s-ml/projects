e8fd085fdaf250a7a736c7eea4b69a13d4cdab49
asml <p80@80q.ru>
2024-01-26 20:09:06 +0300

    +

--- viktorinaonlinebot/src/components/pages/group/SettingsGroup.tsx
+++ viktorinaonlinebot/src/components/pages/group/SettingsGroup.tsx
@@ -1,13 +1,14 @@
-import {
-  useGetInfoGroupsQuery,
-} from "../../store/api/group.api";
+import { useGetInfoGroupsQuery } from "../../store/api/group.api";
 import TypeQuestionGroup from "./sittingsGroup/TypeQuestionGroup";
 import CategoryQuestionGroup from "./sittingsGroup/CategoryQuestionGroup";
 import PeriodQuestionGroup from "./sittingsGroup/PeriodQuestionGroup";
 import NotactiveSittings from "./sittingsGroup/NotactiveSittings";
 import { useAppSelector } from "../../store";
 import { selectModalData } from "../../store/api/modal.slice";
-import { useCountAvailableQuestionQuery, useCountPublishedQuestionQuery } from "../../store/api/question/question.api";
+import {
+  useCountAvailableQuestionQuery,
+  useCountPublishedQuestionQuery,
+} from "../../store/api/question/question.api";
 
 interface NoActive {
   text: string;
@@ -29,8 +30,10 @@ export default function SettingsGroup({}) {
   const group = useAppSelector(selectModalData);
 
   const { data: dataGroupInfo } = useGetInfoGroupsQuery(group);
-  const { data: countAvailableQuestion } = useCountAvailableQuestionQuery(group);
-  const { data: countPublishedQuestion } = useCountPublishedQuestionQuery(group);
+  const { isSuccess: successtAvailable, data: countAvailableQuestion } =
+    useCountAvailableQuestionQuery(group);
+  const { isSuccess: successtPublished, data: countPublishedQuestion } =
+    useCountPublishedQuestionQuery(group);
 
   return (
     <>
@@ -39,7 +42,11 @@ export default function SettingsGroup({}) {
           <h3 className="text-sm font-medium text-[var(--tg-theme-text-color)] text-left">
             Настройки викторины в группе "{dataGroupInfo.title}"
           </h3>
-          <h2>Опубликовано вопросов {countPublishedQuestion} из {countAvailableQuestion}</h2>
+          {successtAvailable && successtPublished && (
+            <h2>
+              Опубликовано вопросов {countPublishedQuestion} из {countAvailableQuestion}
+            </h2>
+          )}
           <ul
             role="list"
             className="mt-4 divide-y divide-[var(--tg-theme-hint-color)]"
