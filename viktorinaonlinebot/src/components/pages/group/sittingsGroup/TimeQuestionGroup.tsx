5546155c18554a64f9f535e13c7eb3d7d5c14228
asml <p80@80q.ru>
2024-01-26 22:24:42 +0300

    +

--- viktorinaonlinebot/src/components/pages/group/sittingsGroup/TimeQuestionGroup.tsx
+++ viktorinaonlinebot/src/components/pages/group/sittingsGroup/TimeQuestionGroup.tsx
@@ -101,7 +101,7 @@ export default function TimeQuestionGroup() {
                   {allTimes.map((time) => (
                     <button
                       type="button"
-                      className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+                      className="flex items-center justify-center rounded-md py-2 px-2 text-xs cursor-pointer"
                     >
                       {time.name}
                     </button>
