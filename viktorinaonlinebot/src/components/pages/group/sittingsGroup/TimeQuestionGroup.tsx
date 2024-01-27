1313236f3ef20087908686aaa116d9e769eeaa57
asml <p80@80q.ru>
2024-01-27 21:57:00 +0300

    +

--- viktorinaonlinebot/src/components/pages/group/sittingsGroup/TimeQuestionGroup.tsx
+++ viktorinaonlinebot/src/components/pages/group/sittingsGroup/TimeQuestionGroup.tsx
@@ -23,44 +23,52 @@ interface ITimesInteface {
 export default function TimeQuestionGroup() {
   const allTimes = [
     { id: 0, time: 0, name: "00:00", active: false, disabled: false },
-    { id: 1, time: 0, name: "01:00", active: false, disabled: false },
-    { id: 2, time: 0, name: "02:00", active: false, disabled: false },
-    { id: 3, time: 0, name: "03:00", active: false, disabled: false },
-    { id: 4, time: 0, name: "04:00", active: false, disabled: false },
-    { id: 5, time: 0, name: "05:00", active: false, disabled: false },
-    { id: 6, time: 0, name: "06:00", active: false, disabled: false },
-    { id: 7, time: 0, name: "07:00", active: false, disabled: false },
-    { id: 8, time: 0, name: "08:00", active: false, disabled: false },
-    { id: 9, time: 0, name: "09:00", active: false, disabled: false },
-    { id: 10, time: 0, name: "10:00", active: false, disabled: false },
-    { id: 11, time: 0, name: "11:00", active: false, disabled: false },
-    { id: 12, time: 0, name: "12:00", active: false, disabled: false },
-    { id: 13, time: 0, name: "13:00", active: false, disabled: false },
-    { id: 14, time: 0, name: "14:00", active: false, disabled: false },
-    { id: 15, time: 0, name: "15:00", active: false, disabled: false },
-    { id: 16, time: 0, name: "16:00", active: false, disabled: false },
-    { id: 17, time: 0, name: "17:00", active: false, disabled: false },
-    { id: 18, time: 0, name: "18:00", active: false, disabled: false },
-    { id: 19, time: 0, name: "19:00", active: false, disabled: false },
-    { id: 20, time: 0, name: "20:00", active: false, disabled: false },
-    { id: 21, time: 0, name: "21:00", active: false, disabled: false },
-    { id: 22, time: 0, name: "22:00", active: false, disabled: false },
-    { id: 23, time: 0, name: "23:00", active: false, disabled: false },
+    { id: 1, time: 1, name: "01:00", active: false, disabled: false },
+    { id: 2, time: 2, name: "02:00", active: false, disabled: false },
+    { id: 3, time: 3, name: "03:00", active: false, disabled: false },
+    { id: 4, time: 4, name: "04:00", active: false, disabled: false },
+    { id: 5, time: 5, name: "05:00", active: false, disabled: false },
+    { id: 6, time: 6, name: "06:00", active: false, disabled: false },
+    { id: 7, time: 7, name: "07:00", active: false, disabled: false },
+    { id: 8, time: 8, name: "08:00", active: false, disabled: false },
+    { id: 9, time: 9, name: "09:00", active: false, disabled: false },
+    { id: 10, time: 10, name: "10:00", active: false, disabled: false },
+    { id: 11, time: 11, name: "11:00", active: false, disabled: false },
+    { id: 12, time: 12, name: "12:00", active: false, disabled: false },
+    { id: 13, time: 13, name: "13:00", active: false, disabled: false },
+    { id: 14, time: 14, name: "14:00", active: false, disabled: false },
+    { id: 15, time: 15, name: "15:00", active: false, disabled: false },
+    { id: 16, time: 16, name: "16:00", active: false, disabled: false },
+    { id: 17, time: 17, name: "17:00", active: false, disabled: false },
+    { id: 18, time: 18, name: "18:00", active: false, disabled: false },
+    { id: 19, time: 19, name: "19:00", active: false, disabled: false },
+    { id: 20, time: 20, name: "20:00", active: false, disabled: false },
+    { id: 21, time: 21, name: "21:00", active: false, disabled: false },
+    { id: 22, time: 22, name: "22:00", active: false, disabled: false },
+    { id: 23, time: 23, name: "23:00", active: false, disabled: false },
   ];
   const chat = useAppSelector(selectModalData);
   const { data: GroupDb } = useGetGroupDbQuery(chat);
   const { data: GroupTime } = useGetTimeByIdQuery(GroupDb?.time || 0);
   const [times, setTimes] = useState(0);
 
-  // let hour: number
-  // GroupTime ? hour = GroupTime.period / 1000 : 25
+  let hours: Array<number> = []
+
+  // .push = GroupTime.period / 1000 : 1
+
+  GroupTime ? allTimes.map((time) => {
+    times + (GroupTime.period / 3600) <= 23 ? hours.push(time.time + (GroupTime.period / 3600)) : console.log('> 23')
+  }) : console.log('не GroupTime')
+
+
+  console.log('hours', hours)
 
   function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
   }
 
   let newArr: ITimesInteface[];
-  console.log(times);
+  console.log('times', times);
 
   function hendleTimes(id: number) {
     // console.log(id);
@@ -117,6 +125,7 @@ export default function TimeQuestionGroup() {
                         onClick={() => hendleTimes(time.id)}
                         className={({ active, checked }) =>
                           classNames(
+
                             time.active === true
                               ? "cursor-not-allowed opacity-25"
                               : "cursor-pointer",
