7e33f7bad43c026f3ea045648d451d692c895709
asml <p80@80q.ru>
2024-01-28 00:59:46 +0300

    +

--- viktorinaonlinebot/src/components/pages/group/sittingsGroup/TimeQuestionGroup.tsx
+++ viktorinaonlinebot/src/components/pages/group/sittingsGroup/TimeQuestionGroup.tsx
@@ -118,7 +118,7 @@ export default function TimeQuestionGroup() {
                           : "ring-1 ring-inset ring-[var(--tg-theme-hint-color)] bg-white text-black font-semibold"
                       }
                           ${
-                            time.id < 7
+                            (time.id < 7 && time.id > 22 && nightNode)
                               ? "bg-[var(--tg-theme-hint-color)] text-black font-semibold cursor-not-allowed"
                               : ""
                           }`}
