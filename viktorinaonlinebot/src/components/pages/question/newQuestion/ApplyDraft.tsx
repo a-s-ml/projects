f6417dbe3c99c20a3ae3b89385d1afb54405ef76
asml <p80@80q.ru>
2024-01-29 13:35:36 +0300

    +

--- viktorinaonlinebot/src/components/pages/question/newQuestion/ApplyDraft.tsx
+++ viktorinaonlinebot/src/components/pages/question/newQuestion/ApplyDraft.tsx
@@ -11,53 +11,58 @@ export default function ApplyDraft() {
 
   return (
     <>
-      <div className="flex w-full flex-col items-center space-y-4">
-        <Transition
-          show={show}
-          as={Fragment}
-          enter="transform ease-out duration-300 transition"
-          enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-          enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-            <div className="p-4">
-              <div className="flex items-center">
-                <div className="flex w-0 flex-1 justify-between">
-                  <p className="w-0 flex-1 text-sm font-medium text-gray-900">
-                    Применить несохранённый черновик
-                  </p>
-                  <button
-                    type="button"
-                    className="ml-3 flex-shrink-0 rounded-md bg-white text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Ок
-                  </button>
-                </div>
-                <div className="ml-4 flex flex-shrink-0">
-                  <button
-                    type="button"
-                    className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    onClick={() => {
-                      setShow(false);
-                    }}
-                  >
-                    <span className="sr-only">Close</span>
-                    <XMarkIcon
-                      className="h-5 w-5"
-                      aria-hidden="true"
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 flex items-end px-4 py-36"
+      >
+        <div className="flex w-full flex-col items-center space-y-4">
+          <Transition
+            show={show}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-4">
+                <div className="flex items-center">
+                  <div className="flex w-0 flex-1 justify-between">
+                    <p className="w-0 flex-1 text-sm font-medium text-gray-900">
+                      Применить несохранённый черновик
+                    </p>
+                    <button
+                      type="button"
+                      className="ml-3 flex-shrink-0 rounded-md bg-white text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      Ок
+                    </button>
+                  </div>
+                  <div className="ml-4 flex flex-shrink-0">
+                    <button
+                      type="button"
+                      className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                       onClick={() => {
                         setShow(false);
                       }}
-                    />
-                  </button>
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        onClick={() => {
+                          setShow(false);
+                        }}
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Transition>
+          </Transition>
+        </div>
       </div>
     </>
   );
