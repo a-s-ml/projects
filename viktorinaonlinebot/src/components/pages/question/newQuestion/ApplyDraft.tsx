e829c3695d77bc025e6b1ef25e3715759fedc4e4
asml <p80@80q.ru>
2024-01-29 13:48:09 +0300

    +

--- viktorinaonlinebot/src/components/pages/question/newQuestion/ApplyDraft.tsx
+++ viktorinaonlinebot/src/components/pages/question/newQuestion/ApplyDraft.tsx
@@ -19,9 +19,9 @@ export default function ApplyDraft({applyDraft}: ApplyDraftProps) {
           <Transition
             show={show}
             as={Fragment}
-            enter="transform ease-out duration-300 transition"
-            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            enter="transform ease-out delay-1000 duration-300 transition"
+            enterFrom="translate-y-2 opacity-0"
+            enterTo="translate-y-0 opacity-100"
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
