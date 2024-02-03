5f0ddbd1c6e928a235fa17a2da337eca2b7f9866
asml <p80@80q.ru>
2024-02-03 21:52:44 +0300

    +

--- viktorinaonlinebot/src/components/hooks/useElevator.ts
+++ viktorinaonlinebot/src/components/hooks/useElevator.ts
@@ -10,8 +10,9 @@ export function useElevator() {
   const tg = window.Telegram.WebApp;
   const dispatch = useAppDispatch();
   const pathSlide = useAppSelector(selectSlidePatch);
+  console.log(pathSlide.length);
 
-  // if (level > 0) {
+  // if (level > 0) {s
   //   tg.BackButton.show();
   //   tg.onEvent("backButtonClicked", () => prevLevel());
   // }
