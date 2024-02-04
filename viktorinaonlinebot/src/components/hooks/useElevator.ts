a84d93988a1f5fb70ca3aefb45c8e89d68eaf06c
asml <p80@80q.ru>
2024-02-04 22:18:47 +0300

    +

--- viktorinaonlinebot/src/components/hooks/useElevator.ts
+++ viktorinaonlinebot/src/components/hooks/useElevator.ts
@@ -26,9 +26,8 @@ export function useElevator() {
   function prevLevel() {
     tg.offEvent("backButtonClicked", () => prevLevel);
     tg.BackButton.hide();
-    if (pathSlide.length > 2) toggleSlide();
-    if (pathSlide.length < 3) dispatch(showSlide(false));
-    dispatch(removePatchSlide(pathSlide[pathSlide.length - 1]));
+    toggleSlide();
+    dispatch(removePatchSlide(pathSlide[pathSlide.length]));
   }
 
   return { nextLevel, prevLevel };
