0221f908c0b6fd8b818b524400dd10939d0c567d
asml <p80@80q.ru>
2024-02-04 22:42:11 +0300

    +

--- viktorinaonlinebot/src/components/store/api/slide.slice.ts
+++ viktorinaonlinebot/src/components/store/api/slide.slice.ts
@@ -35,6 +35,7 @@ export const slideSlice = createSlice({
       state.page = state.patch[state.patch.length - 1];
     },
     removePatchSlide: (state, action: PayloadAction<string>) => {
+      console.log("+");
       state.patch = state.patch.filter((i) => i !== action.payload);
       state.page = state.patch[state.patch.length - 1];
     },
