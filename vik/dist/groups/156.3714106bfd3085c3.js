a6e1bf6e1fdb3a6c6f4fb3c7d1f0d78ffe22b653
asml <p80@80q.ru>
2024-02-11 03:09:16 +0300

    +

--- vik/dist/groups/156.3714106bfd3085c3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkgroups=self.webpackChunkgroups||[]).push([[156],{156:(u,e,p)=>{p.r(e),p.d(e,{default:()=>s.c});var s=p(456)}}]);
--- vik/main/src/app/app.tsx
+++ vik/main/src/app/app.tsx
@@ -1,34 +1,15 @@
 import * as React from 'react';
 
-import NxWelcome from './nx-welcome';
-
-import { Link, Route, Routes } from 'react-router-dom';
-
 const Groups = React.lazy(() => import('groups/Module'));
-
 const Answers = React.lazy(() => import('answers/Module'));
-
 const Questions = React.lazy(() => import('questions/Module'));
 
 export function App() {
   return (
     <React.Suspense fallback={null}>
-      <ul>
-        <li>
-          <Link to="/groups">Groups</Link>
-        </li>
-        <li>
-          <Link to="/answers">Answers</Link>
-        </li>
-        <li>
-          <Link to="/questions">Questions</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path="/groups" element={<Groups />} />
-        <Route path="/answers" element={<Answers />} />
-        <Route path="/questions" element={<Questions />} />
-      </Routes>
+      <Groups />
+      <Answers />
+      <Questions />
     </React.Suspense>
   );
 }
