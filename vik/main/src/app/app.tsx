40b615983f5a5732a0daedeae8a509c6ca46b5b9
asml <p80@80q.ru>
2024-02-11 07:06:06 +0300

    +

--- vik/main/src/app/app.tsx
+++ vik/main/src/app/app.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
+import * as React from 'react'
 
-const Groups = React.lazy(() => import('groups/Module'));
-const Answers = React.lazy(() => import('answers/Module'));
-const Questions = React.lazy(() => import('questions/Module'));
-const Viktorins = React.lazy(() => import('viktorins/Module'));
+const Groups = React.lazy(() => import('groups/Module'))
+const Answers = React.lazy(() => import('answers/Module'))
+const Questions = React.lazy(() => import('questions/Module'))
+const Viktorins = React.lazy(() => import('viktorins/Module'))
 
 export function App() {
-  return (
-    <React.Suspense fallback={null}>
-      <Groups />
-      <Answers />
-      <Questions />
-      <Viktorins />
-    </React.Suspense>
-  );
+	return (
+		<React.Suspense fallback={null}>
+			<Groups />
+			<Answers />
+			<Questions />
+			<Viktorins />
+		</React.Suspense>
+	)
 }
 
-export default App;
+export default App
