e12f1a896895410ae28f48d574b0f3541ab6899b
asml <p80@80q.ru>
2024-02-14 14:11:37 +0300

    +

--- vikapp/components/src/lib/Preloader/Preloader.tsx
+++ vikapp/components/src/lib/Preloader/Preloader.tsx
@@ -2,7 +2,7 @@ import styles from './Preloader.module.css';
 
 export const Preloader = () => {
   return (
-    <div className="flex items-center content-center justify-center w-full my-auto">
+    <div className="flex items-center content-center justify-center w-full h-full">
       <span className={styles.loader} />
     </div>
   );
