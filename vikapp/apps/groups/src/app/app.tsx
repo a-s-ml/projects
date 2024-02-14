4eaaa4134093ea4e3363efc6c7da637002e76f30
asml <p80@80q.ru>
2024-02-15 00:42:47 +0300

    +

--- vikapp/apps/groups/src/app/app.tsx
+++ vikapp/apps/groups/src/app/app.tsx
@@ -1,14 +1,8 @@
 import GroupsPage from './components/GroupsPage';
-import { useGroupDispatch, useGroupSelector } from '@store/groups';
-import { selectGroupData, selectGroupSlide, selectGroupType } from './store/slices/groupApp.slice';
+import { storeGroups, useGroupSelector } from '@store/groups';
 
 export function App() {
-  const tg = window.Telegram.WebApp;
-  const dispatch = useGroupDispatch();
-  const slide = useGroupSelector(selectGroupSlide);
-  const data = useGroupSelector(selectGroupData);
-  const type = useGroupSelector(selectGroupType);
-  console.log(data);
+  console.log(useGroupSelector(storeGroups.getState));
   return (
     <>
       <GroupsPage />
