d6929a481a77222cfb7ef246bbc7ce897a94938e
asml <p80@80q.ru>
2024-02-15 00:14:21 +0300

    +

--- vikapp/apps/groups/src/app/app.tsx
+++ vikapp/apps/groups/src/app/app.tsx
@@ -1,6 +1,10 @@
 import GroupsPage from './components/GroupsPage';
+import { useAppDispatch, useAppSelector } from '@store/groups';
+import { selectGroupData } from './store/slices/groupApp.slice';
 
 export function App() {
+  const data = useAppSelector(selectGroupData);
+  console.log(data);
   return (
     <>
       <GroupsPage />
