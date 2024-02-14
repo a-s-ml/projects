6500bee2f01411da8382e5bdc1d6dff8a6f8027f
asml <p80@80q.ru>
2024-02-14 23:04:05 +0300

    +

--- vikapp/apps/groups/src/app/components/GroupsList.tsx
+++ vikapp/apps/groups/src/app/components/GroupsList.tsx
@@ -1,31 +1,28 @@
-import { useGetGroupsQuery } from "@api/group";
-import { Preloader } from "@components";
-import GroupsListItem from "./GroupsListItem";
+import { useGetGroupsQuery } from '@api/group';
+import { Preloader } from '@components';
+import GroupsListItem from './GroupsListItem';
 
 interface GroupsListProps {
   chat: number;
 }
 export default function GroupsList({ chat }: GroupsListProps) {
-  const {
-    isLoading: loadGroups,
-    data: dataGroups,
-    isSuccess: successGroup,
-  } = useGetGroupsQuery(chat);
+  const { isLoading, data, isSuccess } = useGetGroupsQuery(chat);
+  console.log(data);
 
   return (
     <>
-      {loadGroups && <Preloader />}
+      {isLoading && <Preloader />}
       <ul
         role="list"
         className="mt-4 divide-y divide-[var(--tg-theme-hint-color)] pt-9"
       >
-        {dataGroups?.length === 0 && (
+        {data?.length === 0 && (
           <p className="font-light py-2 text-center text-[var(--tg-theme-text-color)]">
             Вы не добавили бота ещё ни в одну группу/канал
           </p>
         )}
-        {successGroup &&
-          dataGroups.map((group) => (
+        {isSuccess &&
+          data.map((group) => (
             <GroupsListItem key={group.id} group={group.chat} />
           ))}
       </ul>
