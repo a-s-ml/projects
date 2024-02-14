32cdbaf3708a1cdf1f790fa25b89056de9e8dc93
asml <p80@80q.ru>
2024-02-14 13:52:20 +0300

    +

--- vikapp/apps/main/src/app/components/HomePage.tsx
+++ vikapp/apps/main/src/app/components/HomePage.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
 import { useAppDispatch } from '@store/main';
 import { Page, MainBlock, HeaderBlock, FooterBlock } from '@components';
 import { showSlide } from '../store/slices/slide.slice';
+import Logo from "../assets/img/Logo.png";
 
 export function HomePage() {
   const dispatch = useAppDispatch();
@@ -9,6 +9,20 @@ export function HomePage() {
     <>
       <Page>
         <HeaderBlock>
+          <div className="text-center">
+            <img
+              className="mx-auto h-12 w-12"
+              src={Logo}
+              alt="https://80q.ru"
+            />
+            <h2 className="mt-2 text-base font-semibold leading-6 text-[var(--tg-theme-accent-text-color)]">
+              ViktorinaOnlineBot
+            </h2>
+            <p className="mt-1 text-sm text-[var(--tg-theme-hint-color)]">
+              Бот проводит викторины в группах/каналах используя собственную
+              базу вопросов и вопросы, заданные участниками викторины
+            </p>
+          </div>
           <p onClick={() => dispatch(showSlide(true))}>Header</p>
         </HeaderBlock>
         <MainBlock>
