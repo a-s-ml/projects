4954231f76ab072d866a31818472ee5215e1a481
asml <p80@80q.ru>
2024-02-15 14:40:22 +0300

    +

--- vikapp/apps/main/src/bootstrap.tsx
+++ vikapp/apps/main/src/bootstrap.tsx
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <Provider store={storeMain}>
+  <Provider store={storeMain}>
     <StrictMode>
       <App />
     </StrictMode>
-  // </Provider>
+  </Provider>
 );
