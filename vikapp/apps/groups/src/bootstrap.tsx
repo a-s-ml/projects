1576471c56293dafba24071197936fa0113f1fde
asml <p80@80q.ru>
2024-02-15 16:13:22 +0300

    +

--- vikapp/apps/groups/src/bootstrap.tsx
+++ vikapp/apps/groups/src/bootstrap.tsx
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <Provider store={storeGroups}>
+  <Provider store={storeGroups}>
     <StrictMode>
       <App />
     </StrictMode>
-  // </Provider>
+  </Provider>
 );
