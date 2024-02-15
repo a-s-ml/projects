2302b0d2d84d67a7d1f349a56e0bf04cb66325fd
asml <p80@80q.ru>
2024-02-15 15:51:05 +0300

    +

--- vikapp/apps/main/src/bootstrap.tsx
+++ vikapp/apps/main/src/bootstrap.tsx
@@ -3,15 +3,15 @@ import * as ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './app/app';
-import { storeMain } from '@store/main';
+import { storeGroups } from '@store/groups';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <Provider store={storeMain}>
+  <Provider store={storeGroups}>
     <StrictMode>
       <App />
     </StrictMode>
-  // </Provider>
+  </Provider>
 );
