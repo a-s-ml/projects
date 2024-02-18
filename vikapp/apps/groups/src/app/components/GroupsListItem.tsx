3c2f5bd5e0fc579902649c7ff8eff86ff2b70b50
asml <p80@80q.ru>
2024-02-19 00:59:25 +0300

    +

--- vikapp/apps/groups/src/app/components/GroupsListItem.tsx
+++ vikapp/apps/groups/src/app/components/GroupsListItem.tsx
@@ -20,21 +20,21 @@ export default function GroupsListItem({ group }: GroupsListItemProps) {
       {errorGroupInfo && <li>error</li>}
       {dataGroupInfo && GroupDb && (
         <li className="cursor-pointer">
-          <SimpleAccordionIcon>
-            {dataGroupInfo.photo?.small_file_id && (
-              <GroupAvatar id={dataGroupInfo.photo?.small_file_id} />
-            )}
-            {!dataGroupInfo.photo?.small_file_id && (
-              <img
-                className="inline-block h-10 w-14 rounded-full"
-                src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
-                alt={dataGroupInfo.title}
-              />
-            )}
-          </SimpleAccordionIcon>
           <SimpleAccordionMain
             description={
               <>
+                <SimpleAccordionIcon>
+                  {dataGroupInfo.photo?.small_file_id && (
+                    <GroupAvatar id={dataGroupInfo.photo?.small_file_id} />
+                  )}
+                  {!dataGroupInfo.photo?.small_file_id && (
+                    <img
+                      className="inline-block h-10 w-14 rounded-full"
+                      src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+                      alt={dataGroupInfo.title}
+                    />
+                  )}
+                </SimpleAccordionIcon>
                 <SimpleAccordionText>
                   <div className="text-sm font-medium text-[var(--tg-theme-text-color)]">
                     {dataGroupInfo.title}
