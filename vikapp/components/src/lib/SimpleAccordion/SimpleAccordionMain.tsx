bd9abf7abcac6dbdc6da6201decd42d1955e990b
asml <p80@80q.ru>
2024-02-20 00:41:13 +0300

    +

--- vikapp/components/src/lib/SimpleAccordion/SimpleAccordionMain.tsx
+++ vikapp/components/src/lib/SimpleAccordion/SimpleAccordionMain.tsx
@@ -21,7 +21,7 @@ export const SimpleAccordionMain = ({
                 <div className="flex-shrink-0 self-center text-right">
                   <ChevronUpIcon
                     className={`h-5 w-5 text-[var(--tg-theme-accent-text-color)] group-hover:text-[var(--tg-theme-text-color)] ${
-                      open ? 'transform rotate-180 transition ease-in-out duration-500' : ''
+                      open ? 'transform rotate-180 transition ease-in-out duration-500' : 'transition ease-in-out duration-500'
                     }`}
                     aria-hidden="true"
                   />
