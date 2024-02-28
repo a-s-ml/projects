368ec0ed2223a806954ee79477be3c54da53b613
asml <p80@80q.ru>
2024-02-28 23:34:08 +0300

    +

--- vikapp/components/src/lib/GlobalHeader.tsx
+++ vikapp/components/src/lib/GlobalHeader.tsx
@@ -4,7 +4,7 @@ interface GlobalHeaderProps {
 
 export const GlobalHeader = ({ children }: GlobalHeaderProps) => {
   return (
-    <header className="absolute inset-x-0 top-0 z-50 bg-[var(--tg-theme-bg-color)] p-4">
+    <header className="absolute inset-x-0 top-0 z-9 bg-[var(--tg-theme-bg-color)] p-4">
       {children}
     </header>
   );
