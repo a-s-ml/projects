6c0084a229bc9b6a319ca8f9b914025f761c1d4f
asml <viktorina.online@yandex.ru>
2024-03-05 18:55:52 +0300

    +

--- vikapp/apps/main/src/app/components/MenuListItem.tsx
+++ vikapp/apps/main/src/app/components/MenuListItem.tsx
@@ -16,7 +16,7 @@ export const MenuListItem = () => {
           className="cursor-pointer"
           onClick={() => {
             setBackButtonState(true);
-            setTypeSlide('item.type');
+            setTypeSlide(item.type);
           }}
         >
           <SimpleListItem>
