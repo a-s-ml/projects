d825f49705ed6d65c74a829cb926357cbe26e606
asml <viktorina.online@yandex.ru>
2024-03-07 18:49:26 +0300

    +

--- vikapp/apps/questions/src/app/components/NewQuestion/CategoryList.tsx
+++ vikapp/apps/questions/src/app/components/NewQuestion/CategoryList.tsx
@@ -3,22 +3,18 @@ import { getQuestionCategory, selectQuestionCategory } from '@slice/questions';
 import { useState } from 'react';
 import { ICategory } from '@models';
 import { useGetCategoryQuery } from '@api/category';
-import {
-  SimpleCategorySelect,
-  SimpleCheckbox,
-  ValidateForm,
-} from '@components';
+import { SimpleCategorySelect, ValidateForm } from '@components';
 
 interface CategoryListProps {
   validate: (b: boolean) => void;
 }
 
-export function CategoryList({ validate }: CategoryListProps) {
+export const CategoryList = ({ validate }: CategoryListProps) => {
+  const dispatch = useQuestionDispatch();
   const questionCategory = useQuestionSelector(selectQuestionCategory);
   console.log('questionCategory', questionCategory);
   const { data: allCategory } = useGetCategoryQuery('');
   console.log('data', allCategory);
-  const dispatch = useQuestionDispatch();
   const [selectedCategory, setCategory] = useState(
     allCategory && questionCategory != 0
       ? allCategory.find((id) => id.id === questionCategory)
@@ -30,30 +26,18 @@ export function CategoryList({ validate }: CategoryListProps) {
     setCategory(cat);
     dispatch(getQuestionCategory(cat.id));
   };
-  async function categoryChanged(check: boolean, category: number) {
-    if (!check) {
-      // await setCategory({ chat, category });
-    }
-    if (check) {
-      // deleteCategory({ chat, category });
-    }
-  }
+
   const [validCategory, setValidCategory] = useState(false);
 
   validCategory ? validate(true) : validate(false);
 
   return (
     <div className="py-2">
-      {allCategory &&
-        allCategory.map((item) => (
-          <SimpleCheckbox
-            key={item.id}
-            data={item}
-            disabled={item.id === 1001 || item.id === 85 ? true : false}
-            checked={false}
-            func={categoryChanged}
-          />
-        ))}
+      <SimpleCategorySelect
+        value={selectedCategory ? selectedCategory : { id: 0, name: ' ' }}
+        func={handleChange}
+        data={allCategory ? allCategory : [{ id: 0, name: ' ' }]}
+      />
       <div className="py-4">
         <ValidateForm
           text={'Выберите подходящую категорию'}
@@ -67,4 +51,6 @@ export function CategoryList({ validate }: CategoryListProps) {
       </div>
     </div>
   );
-}
+};
+
+export default CategoryList;
