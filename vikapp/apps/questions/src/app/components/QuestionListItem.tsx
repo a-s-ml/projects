f5ba6a0fb9fb05e4120a3f9437d6fed24268b948
asml <viktorina.online@yandex.ru>
2024-03-12 00:48:39 +0300

    +

--- vikapp/apps/questions/src/app/components/QuestionListItem.tsx
+++ vikapp/apps/questions/src/app/components/QuestionListItem.tsx
@@ -35,37 +35,39 @@ export const QuestionListItem = ({ chat }: QuestionListItemProps) => {
               }
               content={
                 <>
-                  <ButtonIcon
-                    firstIcon={true}
-                    text={'Модерировать'}
-                    color={'[var(--tg-theme-accent-text-color)]'}
-                    icon={'moderate'}
-                    func={inDevelopment}
-                  />
-                  <ButtonIcon
-                    firstIcon={true}
-                    text={'Статистика'}
-                    color={'[var(--tg-theme-accent-text-color)]'}
-                    icon={'stat'}
-                    func={inDevelopment}
-                  />
-                  <ButtonIcon
-                    firstIcon={true}
-                    text={'Отправить'}
-                    color={'[var(--tg-theme-accent-text-color)]'}
-                    icon={'send'}
-                    func={inDevelopment}
-                  />
-                  <ButtonIconConfirm
-                    firstIcon={true}
-                    text={'Удалить'}
-                    color={'red-500'}
-                    icon={'delete'}
-                    func={deleteQuestion}
-                    textConfirm={`Вы действительно хотите удалить вопрос №${
-                      isSuccess ? data[0].id : 'не success'
-                    }`}
-                  />
+                  <div className={`grid grid-cols-2 gap-2`}>
+                    <ButtonIcon
+                      firstIcon={true}
+                      text={'Модерировать'}
+                      color={'[var(--tg-theme-accent-text-color)]'}
+                      icon={'moderate'}
+                      func={inDevelopment}
+                    />
+                    <ButtonIcon
+                      firstIcon={true}
+                      text={'Статистика'}
+                      color={'[var(--tg-theme-accent-text-color)]'}
+                      icon={'stat'}
+                      func={inDevelopment}
+                    />
+                    <ButtonIcon
+                      firstIcon={true}
+                      text={'Отправить'}
+                      color={'[var(--tg-theme-accent-text-color)]'}
+                      icon={'send'}
+                      func={inDevelopment}
+                    />
+                    <ButtonIconConfirm
+                      firstIcon={true}
+                      text={'Удалить'}
+                      color={'red-500'}
+                      icon={'delete'}
+                      func={deleteQuestion}
+                      textConfirm={`Вы действительно хотите удалить вопрос №${
+                        isSuccess ? data[0].id : 'не success'
+                      }`}
+                    />
+                  </div>
                 </>
               }
             />
