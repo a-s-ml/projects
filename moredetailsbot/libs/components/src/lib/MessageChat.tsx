36a0719610f4f58837bc48f696175c73dd5237d0
asml <viktorina.online@yandex.ru>
2024-03-16 21:48:58 +0300

    +

--- moredetailsbot/libs/components/src/lib/MessageChat.tsx
+++ moredetailsbot/libs/components/src/lib/MessageChat.tsx
@@ -9,9 +9,9 @@ export function MessageChat({ name, text, time }: MessageChatProps) {
     <div>
       <div className="flex mb-2">
         <div className="rounded py-2 px-3 bg-slate-300">
-          <p className="text-sm text-orange">{name}</p>
+          <p className="text-sm text-orange">Имя: {name}</p>
           <p className="text-sm mt-1">{text}</p>
-          <p className="text-right text-xs text-grey-dark mt-1">{time}</p>
+          <p className="text-right text-xs text-grey-dark mt-1">{Date.now()}</p>
         </div>
       </div>
     </div>
