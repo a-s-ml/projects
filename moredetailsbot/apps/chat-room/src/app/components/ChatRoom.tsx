6dcebc3b07ba900e0dcde5b92ec9d7cd82604ed6
asml <viktorina.online@yandex.ru>
2024-03-16 06:05:40 +0300

    +

--- moredetailsbot/apps/chat-room/src/app/components/ChatRoom.tsx
+++ moredetailsbot/apps/chat-room/src/app/components/ChatRoom.tsx
@@ -1,4 +1,5 @@
 import ChatPanel from 'libs/components/src/lib/ChatPanel';
+import MessageChat from 'libs/components/src/lib/MessageChat';
 import { useEffect, useState } from 'react';
 import { Socket, io } from 'socket.io-client';
 
@@ -6,9 +7,16 @@ export interface ChatRoomProps {
   accessToken: string | undefined;
 }
 
+type Event = {
+  id: number;
+  user: number;
+  chat: number;
+  text: string;
+};
+
 export const ChatRoom = ({ accessToken }: ChatRoomProps) => {
   console.log('accessToken', accessToken);
-  const [state, setState] = useState([]);
+  const [state, setState] = useState<Event[]>([]);
   useEffect(() => {
     const socket = io('https://api80q.ru/chat', {
       auth: {
@@ -17,7 +25,10 @@ export const ChatRoom = ({ accessToken }: ChatRoomProps) => {
       transports: ['websocket', 'polling'],
     });
     const listener = (...args: any[]) => {
-      console.log(args);
+      setState((prevState) => ({
+        ...prevState,
+        args,
+      }));
     };
 
     socket.on('chat_updated', listener);
@@ -28,12 +39,15 @@ export const ChatRoom = ({ accessToken }: ChatRoomProps) => {
   return (
     <p>
       <ChatPanel>
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-        <p>4</p>
         {state.map((message) => (
-          <p>{message}</p>
+          <>
+            <MessageChat
+              key={message.id}
+              name={String(message.user)}
+              text={message.text}
+              time={String(message.chat)}
+            />
+          </>
         ))}
       </ChatPanel>
     </p>
