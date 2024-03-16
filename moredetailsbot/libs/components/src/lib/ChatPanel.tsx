6cffd67ad2ca41912a83ee264c061ae506d49c7c
asml <viktorina.online@yandex.ru>
2024-03-17 02:56:55 +0300

    +

--- moredetailsbot/libs/components/src/lib/ChatPanel.tsx
+++ moredetailsbot/libs/components/src/lib/ChatPanel.tsx
@@ -4,7 +4,7 @@ export interface ChatPanelProps {
 
 export function ChatPanel({ children }: ChatPanelProps) {
   return (
-    <div className="flex-1 overflow-auto bg-orange-200">
+    <div className="flex-1 overflow-auto w-screen h-screen">
       <div className="py-2 px-3">{children}</div>
     </div>
   );
--- moredetailsbot/libs/components/src/lib/MessageChat.tsx
+++ moredetailsbot/libs/components/src/lib/MessageChat.tsx
@@ -8,10 +8,10 @@ export function MessageChat({ name, text, time }: MessageChatProps) {
   return (
     <div>
       <div className="flex mb-2">
-        <div className="rounded py-2 px-3 bg-slate-300">
-          <p className="text-sm text-orange">Имя: {name}</p>
-          <p className="text-sm mt-1">{text}</p>
-          <p className="text-right text-xs text-grey-dark mt-1">{time}</p>
+        <div className="rounded py-2 px-3 bg-[var(--tg-theme-hint-color)]">
+          <p className="text-sm text-[var(--tg-theme-hint-color)]">Имя: {name}</p>
+          <p className="text-sm mt-1 text-[var(--tg-theme-accent-text-color)]">{text}</p>
+          <p className="text-right text-xs text-[var(--tg-theme-hint-color)] mt-1">{time}</p>
         </div>
       </div>
     </div>
