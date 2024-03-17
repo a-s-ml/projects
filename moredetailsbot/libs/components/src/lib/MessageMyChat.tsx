a3505c05f9156b67ba7cb79443fce23f421a404b
asml <viktorina.online@yandex.ru>
2024-03-17 22:28:44 +0300

    +

--- moredetailsbot/libs/components/src/lib/MessageMyChat.tsx
+++ moredetailsbot/libs/components/src/lib/MessageMyChat.tsx
@@ -6,7 +6,7 @@ export function MessageMyChat({ text }: MessageMyChatProps) {
   const now = new Date();
   return (
     <div>
-      <div className="flex justify-end mb-2">
+      <div className="flex justify-end mb-2 max-w-[75%]">
         <div className="rounded py-2 px-3 bg-[var(--tg-theme-button-color)]">
           <p className="text-sm mt-4 text-[var(--tg-theme-text-color)]">
             {text}
