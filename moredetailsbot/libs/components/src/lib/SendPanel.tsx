3c116f35fbd6d34c3ee9dba0946a9721a79f6bd2
asml <viktorina.online@yandex.ru>
2024-03-17 22:33:04 +0300

    +

--- moredetailsbot/libs/components/src/lib/SendPanel.tsx
+++ moredetailsbot/libs/components/src/lib/SendPanel.tsx
@@ -17,16 +17,16 @@ export function SendPanel({
 }: SendPanelProps) {
   return (
     <div className="absolute bt-2 b-[--tg-theme-hint-color] bottom-0 pt-4 pb-5 flex items-center space-x-3 justify-center bg-[var(--tg-theme-bg-color)]">
-      <div className="px-5 ">
-        <FaceSmileIcon className="h-6 w-6 text-[var(--tg-theme-text-color)]" />
-      </div>
       <input
-        className="w-1/2 border rounded px-2 py-2"
+        className="w-2/3 border rounded px-2 py-2 bg-[--tg-theme-hint-color] text-"
         type="text"
         value={message}
         onChange={handleChange}
       />
-      <div className="px-5 " onClick={() => handleSubmit(message)}>
+      <div className="px-1">
+        <FaceSmileIcon className="h-6 w-6 text-[var(--tg-theme-text-color)]" />
+      </div>
+      <div className="px-1" onClick={() => handleSubmit(message)}>
         <ArrowRightCircleIcon className="h-6 w-6 text-[var(--tg-theme-text-color)]" />
       </div>
     </div>
