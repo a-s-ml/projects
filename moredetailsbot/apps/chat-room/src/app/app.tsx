79496945ba978b8efa35163dc0b0494c0b253edd
asml <viktorina.online@yandex.ru>
2024-03-18 01:41:52 +0300

    +

--- moredetailsbot/apps/chat-room/src/app/app.tsx
+++ moredetailsbot/apps/chat-room/src/app/app.tsx
@@ -1,8 +1,16 @@
 import { Provider } from 'react-redux';
 import { storeChatRoom } from './store';
 import MainPage from './components/MainPage';
+import React from 'react';
 
 export const App = () => {
+  const tg = window.Telegram.WebApp;
+  
+  React.useEffect(() => {
+    tg.expand();
+    tg.ready();
+  }, []);
+
   return (
     <Provider store={storeChatRoom}>
       <MainPage />
--- moredetailsbot/libs/components/src/lib/ChatPanel.tsx
+++ moredetailsbot/libs/components/src/lib/ChatPanel.tsx
@@ -5,7 +5,7 @@ export interface ChatPanelProps {
 export function ChatPanel({ children }: ChatPanelProps) {
   return (
     <div className="flex overflow-auto w-screen h-screen">
-      <div className="pt-2 px-3 pb-32">{children}</div>
+      {children}
     </div>
   );
 }
