1a01c7394b4148a1c39ac84eb2a371d23a7c9251
asml <viktorina.online@yandex.ru>
2024-03-19 19:21:57 +0300

    +

--- moredetailsbot/libs/components/src/lib/SimpleButton.tsx
+++ moredetailsbot/libs/components/src/lib/SimpleButton.tsx
@@ -15,8 +15,8 @@ export const SimpleButton = ({ content, icon, click }: SimpleButtonProps) => {
       type="button"
       className="inline-flex items-center gap-x-2 rounded-md bg-[var(--tg-theme-accent-text-color)] px-3.5 py-2.5 text-sm font-semibold text-[var(--tg-theme-accent-text-color)] shadow-sm hover:bg-[var(--tg-theme-accent-text-color)]"
     >
-      <cur.component className={`h-5 w-5 text-tg-theme-button-text-color`} aria-hidden="true" />
-      <div className={`text-sm font-medium text-tg-theme-button-text-color`}>
+      <cur.component className={`h-5 w-5 text-[var(--tg-theme-button-text-color)]`} aria-hidden="true" />
+      <div className={`text-sm font-medium text-[var(--tg-theme-button-text-color)]`}>
         <b>{content}</b>
       </div>
     </button>
