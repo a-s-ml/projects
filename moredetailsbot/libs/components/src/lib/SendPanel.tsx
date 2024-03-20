ffa137aeb2b23e4f099fcf509ec1f62e14c651a1
asml <viktorina.online@yandex.ru>
2024-03-20 05:46:08 +0300

    +

--- moredetailsbot/libs/components/src/lib/SendPanel.tsx
+++ moredetailsbot/libs/components/src/lib/SendPanel.tsx
@@ -13,29 +13,30 @@ export function SendPanel({
   handleChange,
   message,
 }: SendPanelProps) {
-  // const MIN_TEXTAREA_HEIGHT = 32;
-  // const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const MIN_TEXTAREA_HEIGHT = 32;
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  // useLayoutEffect(() => {
-  //   if (textareaRef.current) {
-  //     textareaRef.current.focus();
-  //     textareaRef.current.style.height = 'inherit';
-  //     textareaRef.current.style.height = `${Math.max(
-  //       textareaRef.current.scrollHeight,
-  //       MIN_TEXTAREA_HEIGHT
-  //     )}px`;
-  //   }
-  // }, [message]);
+  useLayoutEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.focus();
+      textareaRef.current.style.height = 'inherit';
+      textareaRef.current.style.height = `${Math.max(
+        textareaRef.current.scrollHeight,
+        MIN_TEXTAREA_HEIGHT
+      )}px`;
+    }
+  }, [message]);
 
   return (
     <div className="flex py-6 justify-center items-start bg-[var(--tg-theme-bg-color)]">
       <textarea
-        className="w-2/3 border rounded px-2 py-2 bg-[--tg-theme-hint-color] text-"
+        className="w-2/3 border-box rounded px-2 py-2 bg-[--tg-theme-hint-color] text-[var(--tg-theme-bg-color)] h-10 min-h-[40px]"
         // ref={textareaRef}
         // style={{
         //   minHeight: MIN_TEXTAREA_HEIGHT,
         //   resize: 'none',
         // }}
+        style={{ width: `${message.length}ch` }}
         value={message}
         onChange={handleChange}
       />
