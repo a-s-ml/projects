7eb1bd24e8eff3dee1703286bbae5bdf7b74b9f3
asml <viktorina.online@yandex.ru>
2024-03-21 07:31:12 +0300

    +

--- moredetailsbot/apps/chat-room/src/app/context/peer.ts
+++ moredetailsbot/apps/chat-room/src/app/context/peer.ts
@@ -1,42 +1,45 @@
 class PeerService {
-    peer: any;
-    constructor() {
-      if (!this.peer) {
-        this.peer = new RTCPeerConnection({
-          iceServers: [
-            {
-              urls: [
-                "stun:stun.l.google.com:19302",
-                "stun:global.stun.twilio.com:3478",
-              ],
-            },
-          ],
-        });
-      }
-    }
-  
-    async getAnswer(offer: RTCSessionDescriptionInit) {
-      if (this.peer) {
-        await this.peer.setRemoteDescription(offer);
-        const ans = await this.peer.createAnswer();
-        await this.peer.setLocalDescription(new RTCSessionDescription(ans));
-        return ans;
-      }
-    }
-  
-    async setLocalDescription(ans: RTCSessionDescriptionInit) {
-      if (this.peer) {
-        await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
-      }
-    }
-  
-    async getOffer() {
-      if (this.peer) {
-        const offer = await this.peer.createOffer();
-        await this.peer.setLocalDescription(new RTCSessionDescription(offer));
-        return offer;
-      }
+  peer: any;
+  constructor() {
+    if (!this.peer) {
+      this.peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            // urls: [
+            //   "stun:stun.l.google.com:19302",
+            //   "stun:global.stun.twilio.com:3478",
+            // ],
+            urls: ['stun:freestun.net:5350', 'turns:freestun.tel:5350'],
+            username: 'free',
+            credential: 'free',
+          },
+        ],
+      });
     }
   }
-  
-  export default new PeerService();
+
+  async getAnswer(offer: RTCSessionDescriptionInit) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(offer);
+      const ans = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+      return ans;
+    }
+  }
+
+  async setLocalDescription(ans: RTCSessionDescriptionInit) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
+    }
+  }
+
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+      return offer;
+    }
+  }
+}
+
+export default new PeerService();
